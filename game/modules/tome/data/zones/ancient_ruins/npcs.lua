9ce0336b82a7bc8baae62a45a105cc2c9f409a24
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2009-11-23 23:49:04 +0000

    change
    git-svn-id: http://svn.net-core.org/repos/t-engine4@26 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/zones/ancient_ruins/npcs.lua
+++ game/modules/tome/data/zones/ancient_ruins/npcs.lua
@@ -8,6 +8,7 @@ return {
 	mana = 1000,
 	energy = { mod=0.8 },
 	has_blood = true,
+	stats = { str=15, dex=8, mag=12, },
 	combat = { dam=8, atk=10, apr=2, def=4, armor=6},
 },
 {
@@ -17,7 +18,7 @@ return {
 	life = 30,
 	mana = 1000,
 	energy = { mod=0.3 },
-	has_blood = true,
+	has_blood = {nb=3, color={50,255,120}},
 	combat = { dam=5, atk=6, def=2, apr=1, armor=2},
 },
 
