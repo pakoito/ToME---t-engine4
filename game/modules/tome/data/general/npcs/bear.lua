2a1eb4ca0a26e5b8bc597e0a32184391bf1b8790
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-02-03 10:08:18 +0000

    fix
    git-svn-id: http://svn.net-core.org/repos/t-engine4@311 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/general/npcs/bear.lua
+++ game/modules/tome/data/general/npcs/bear.lua
@@ -1,3 +1,5 @@
+--updated: 7:59 AM 1/30/2010
+
 local Talents = require("engine.interface.ActorTalents")
 
 newEntity{
@@ -14,9 +16,9 @@ newEntity{
 	stats = { str=18, dex=13, mag=5, con=15 },
 
 	combat_armor = 1, combat_def = 1,
-	combat = { dam=resolvers.rngavg(12,25), atk=10, apr=10, physspeed=2 },
-	life_rating = 13,
-	tmasteries = resolvers.tmasteries{ ["technique/other"]=0.25 },
+	combat = { dam=resolvers.rngavg(12,25), atk=10, apr=3, physspeed=2 },
+	life_rating = 12,
+	tmasteries = resolvers.tmasteries{ ["physical/other"]=0.25 },
 
 	resists = { [DamageType.FIRE] = 20, [DamageType.COLD] = 20, [DamageType.POISON] = 20 },
 }
@@ -26,17 +28,17 @@ newEntity{ base = "BASE_NPC_BEAR",
 	desc = [[The weakest of bears, covered in brown shaggy fur.]],
 	level_range = {5, 50}, exp_worth = 1,
 	rarity = 7,
-	max_life = resolvers.rngavg(100,150),
+	max_life = resolvers.rngavg(80,90),
 	combat_armor = 7, combat_def = 3,
 	talents = resolvers.talents{ [Talents.T_STUN]=1 },
 }
 
 newEntity{ base = "BASE_NPC_BEAR",
-	name = "black bear", color=colors.BLACK,
-	desc = [[Do you smell like honey, 'cause this bear wants honey.]],
+	name = "black bear", color={0,0,0},
+	desc = [[Do you smell like honey? 'cause this bear wants honey.]],
 	level_range = {6, 50}, exp_worth = 1,
 	rarity = 7,
-	max_life = resolvers.rngavg(120,150),
+	max_life = resolvers.rngavg(90,100),
 	combat_armor = 8, combat_def = 3,
 	talents = resolvers.talents{ [Talents.T_STUN]=1 },
 }
@@ -46,20 +48,20 @@ newEntity{ base = "BASE_NPC_BEAR",
 	desc = [[It has come down from its cave foraging for food. Unfortunately, it found you.]],
 	level_range = {7, 50}, exp_worth = 1,
 	rarity = 8,
-	max_life = resolvers.rngavg(130,150),
+	max_life = resolvers.rngavg(100,110),
 	combat_armor = 9, combat_def = 4,
-	combat = { dam=resolvers.rngavg(13,17), atk=7, apr=7, physspeed=2 },
+	combat = { dam=resolvers.rngavg(13,17), atk=7, apr=2, physspeed=2 },
 	talents = resolvers.talents{ [Talents.T_STAMINA_POOL]=1, [Talents.T_STUN]=2, [Talents.T_KNOCKBACK]=1,},
 }
 
 newEntity{ base = "BASE_NPC_BEAR",
 	name = "war bear", color=colors.DARK_UMBER,
 	desc = [[Bears with tusks, trained to kill.]],
-	level_range = {8, 50}, exp_worth = 1,
+	level_range = {7, 50}, exp_worth = 1,
 	rarity = 8,
-	max_life = resolvers.rngavg(140,150),
+	max_life = resolvers.rngavg(100,120),
 	combat_armor = 9, combat_def = 4,
-	combat = { dam=resolvers.rngavg(13,17), atk=10, apr=7, physspeed=2 },
+	combat = { dam=resolvers.rngavg(13,17), atk=10, apr=3, physspeed=2 },
 	talents = resolvers.talents{ [Talents.T_STAMINA_POOL]=1, [Talents.T_STUN]=2, [Talents.T_KNOCKBACK]=1,},
 }
 
@@ -68,8 +70,20 @@ newEntity{ base = "BASE_NPC_BEAR",
 	desc = [[A huge, beastly bear, more savage than most of its kind.]],
 	level_range = {10, 50}, exp_worth = 1,
 	rarity = 9,
-	max_life = resolvers.rngavg(150,170),
+	max_life = resolvers.rngavg(110,120),
 	combat_armor = 10, combat_def = 5,
-	combat = { dam=resolvers.rngavg(15,20), atk=10, apr=7, physspeed=2 },
+	combat = { dam=resolvers.rngavg(15,20), atk=10, apr=3, physspeed=2 },
 	talents = resolvers.talents{ [Talents.T_STAMINA_POOL]=1, [Talents.T_STUN]=3, [Talents.T_KNOCKBACK]=2,},
+}
+
+newEntity{ base = "BASE_NPC_BEAR",
+	name = "polar bear", color=colors.WHITE,
+	desc = [[This huge white bear has wandered south in search of food.]],
+	level_range = {12, 50}, exp_worth = 1,
+	rarity = 11,
+	max_life = resolvers.rngavg(110,120),
+	combat_armor = 10, combat_def = 7,
+	combat = { dam=resolvers.rngavg(15,20), atk=12, apr=3, physspeed=2 },
+	talents = resolvers.talents{ [Talents.T_STAMINA_POOL]=1, [Talents.T_STUN]=3, [Talents.T_KNOCKBACK]=2,},
+	resists = { [DamageType.COLD] = 100, },
 }
