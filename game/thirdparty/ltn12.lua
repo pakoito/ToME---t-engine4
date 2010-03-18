278b023fc2c317c405cbb011d90444ed3e56593c
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-03-18 13:21:09 +0000

    remove old style vararg (arg instead of ...) because of luajit
    git-svn-id: http://svn.net-core.org/repos/t-engine4@440 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/thirdparty/ltn12.lua
+++ game/thirdparty/ltn12.lua
@@ -38,6 +38,7 @@ end
 -- chains a bunch of filters together
 -- (thanks to Wim Couwenberg)
 function filter.chain(...)
+    local arg = {...}
     local n = table.getn(arg)
     local top, index = 1, 1
     local retry = ""
@@ -186,6 +187,7 @@ end
 -- other, as if they were concatenated
 -- (thanks to Wim Couwenberg)
 function source.cat(...)
+    local arg = {...}
     local src = table.remove(arg, 1)
     return function()
         while src do
