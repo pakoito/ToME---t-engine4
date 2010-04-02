6e28a109fadc176c7adff5ba513e07c042a215e5
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-04-02 12:31:31 +0000

    telepathy does not prevent rest/run
    git-svn-id: http://svn.net-core.org/repos/t-engine4@488 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/class/Player.lua
+++ game/modules/tome/class/Player.lua
@@ -201,7 +201,7 @@ end
 local function spotHostiles(self)
 	local seen = false
 	-- Check for visible monsters, only see LOS actors, so telepathy wont prevent resting
-	core.fov.calc_circle(self.x, self.y, 20, function(_, x, y) game.level.map:opaque(x, y) end, function(_, x, y)
+	core.fov.calc_circle(self.x, self.y, 20, function(_, x, y) return game.level.map:opaque(x, y) end, function(_, x, y)
 		local actor = game.level.map(x, y, game.level.map.ACTOR)
 		if actor and self:reactionToward(actor) < 0 and self:canSee(actor) and game.level.map.seens(x, y) then seen = true end
 	end, nil)
