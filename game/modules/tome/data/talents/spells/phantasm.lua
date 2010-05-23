61a90b3f3364e161d8740d7e133eb63dab2b6200
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-05-23 20:33:45 +0000

    scroll of light/illumination spell dont let a dark spot on the player
    git-svn-id: http://svn.net-core.org/repos/t-engine4@677 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/spells/phantasm.lua
+++ game/modules/tome/data/talents/spells/phantasm.lua
@@ -25,9 +25,10 @@ newTalent{
 	mana = 5,
 	cooldown = 14,
 	action = function(self, t)
-		local tg = {type="ball", range=0, friendlyfire=false, radius=5 + self:getTalentLevel(t), talent=t}
+		local tg = {type="ball", range=0, friendlyfire=true, radius=5 + self:getTalentLevel(t), talent=t}
 		self:project(tg, self.x, self.y, DamageType.LIGHT, 1)
 		if self:getTalentLevel(t) >= 3 then
+			tg.friendlyfire = false
 			self:project(tg, self.x, self.y, DamageType.BLIND, 3 + self:getTalentLevel(t))
 		end
 		game:playSoundNear(self, "talents/heal")
