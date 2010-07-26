c2558183a6c3472700943f12935ed9aa6f44b3a7
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-07-26 16:34:07 +0000

    unlock pyromancers
    git-svn-id: http://svn.net-core.org/repos/t-engine4@940 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/texts/unlock-mage_pyromancer.lua
@@ -0,0 +1,38 @@
+-- ToME - Tales of Middle-Earth
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+return "New Class: #LIGHT_GREEN#Pyromancer",
+[[
+Magic has not disappeared from Middle-earth with the downfall of Sauron and the departing of Gandalf the Grey.
+
+Before the war of the ring, Gandalf, fearing that he might die leaving his task incomplete, started a secret plan with a group of trusted elves and men to form a place of knowledge, hidden from all.
+This place is Angolwen, the City of Magic, and for over one hundred years has taught magic to preserve and expand it.
+
+You have uncovered the secrets of the fire magic and can now create new characters with the #LIGHT_GREEN#Pyromancer class#WHITE#.
+
+Pyromancers are specialized archmages, attuned to the magical properties of fire.
+Class features:#YELLOW#
+- Cast potent fire spells to burn your foes to ashes.
+- Set all your foes ablaze and make the fire consume them.
+- Learn to master the Wildfire, a fire so hot it can burn even those resistant to normal fire.
+- Manipulate the forces of magic themselves#WHITE#
+
+All mages use mana to cast their spells.
+It slowly replenishes over time.
+]]
