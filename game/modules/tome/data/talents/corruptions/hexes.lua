04007eb7ef780a9be034a6cf12dbcf1f5d33a6e1
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-09-02 23:38:45 +0000

    more
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1129 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/talents/corruptions/hexes.lua
@@ -0,0 +1,130 @@
+-- ToME - Tales of Middle-Earth
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+newTalent{
+	name = "Pacification Hex",
+	type = {"corruption/hexes", 1},
+	require = corrs_req1,
+	points = 5,
+	cooldown = 20,
+	vim = 20,
+	range = 20,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t), talent=t}
+		local x, y = self:getTarget(tg)
+		if not x or not y then return nil end
+		self:project(tg, x, y, function(tx, ty)
+			local target = game.level.map(tx, ty, Map.ACTOR)
+			if not target then return end
+			if target:checkHit(self:combatSpellpower(), target:combatSpellResist(), 0, 95, 15) then
+				target:setEffect(target.EFF_CURSE_DEFENSELESSNESS, 10, {power=self:combatTalentSpellDamage(t, 30, 60)})
+			end
+		end)
+		game:playSoundNear(self, "talents/slime")
+		return true
+	end,
+	info = function(self, t)
+		return ([[Curses your target, decreasing its defense and saves by %d.
+		The defense and saves will decrease with Magic stat.]]):format(self:combatTalentSpellDamage(t, 10, 30))
+	end,
+}
+
+newTalent{
+	name = "Burning Hex",
+	type = {"corruption/hexes", 2},
+	require = corrs_req2,
+	points = 5,
+	cooldown = 20,
+	vim = 20,
+	range = 20,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t), talent=t}
+		local x, y = self:getTarget(tg)
+		if not x or not y then return nil end
+		self:project(tg, x, y, function(tx, ty)
+			local target = game.level.map(tx, ty, Map.ACTOR)
+			if not target then return end
+			if target:checkHit(self:combatSpellpower(), target:combatSpellResist(), 0, 95, 15) then
+				target:setEffect(target.EFF_CURSE_IMPOTENCE, 10, {power=self:combatTalentSpellDamage(t, 10, 30)})
+			end
+		end)
+		game:playSoundNear(self, "talents/slime")
+		return true
+	end,
+	info = function(self, t)
+		return ([[Curses your target, decreasing all damage done by %d%%.
+		The damage will decrease with Magic stat.]]):format(self:combatTalentSpellDamage(t, 10, 30))
+	end,
+}
+
+newTalent{
+	name = "Empathic Hex",
+	type = {"corruption/hexes", 3},
+	require = corrs_req3,
+	points = 5,
+	cooldown = 20,
+	vim = 20,
+	range = 20,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t), talent=t}
+		local x, y = self:getTarget(tg)
+		if not x or not y then return nil end
+		self:project(tg, x, y, function(tx, ty)
+			local target = game.level.map(tx, ty, Map.ACTOR)
+			if not target then return end
+			if target:checkHit(self:combatSpellpower(), target:combatSpellResist(), 0, 95, 15) then
+				target:setEffect(target.EFF_CURSE_DEATH, 10, {src=self, dam=self:combatTalentSpellDamage(t, 10, 70)})
+			end
+		end)
+		game:playSoundNear(self, "talents/slime")
+		return true
+	end,
+	info = function(self, t)
+		return ([[Curses your target, stopping any natural healing and dealing %0.2f darkness damage each turn.
+		The resistances will decrease with Magic stat.]]):format(self:combatTalentSpellDamage(t, 10, 70))
+	end,
+}
+
+newTalent{
+	name = "Domination Hex",
+	type = {"corruption/hexes", 4},
+	require = corrs_req4,
+	points = 5,
+	cooldown = 20,
+	vim = 20,
+	range = 20,
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t), talent=t}
+		local x, y = self:getTarget(tg)
+		if not x or not y then return nil end
+		self:project(tg, x, y, function(tx, ty)
+			local target = game.level.map(tx, ty, Map.ACTOR)
+			if not target then return end
+			if target:checkHit(self:combatSpellpower(), target:combatSpellResist(), 0, 95, 15) then
+				target:setEffect(target.EFF_CURSE_VULNERABILITY, 10, {power=self:combatTalentSpellDamage(t, 10, 60)})
+			end
+		end)
+		game:playSoundNear(self, "talents/slime")
+		return true
+	end,
+	info = function(self, t)
+		return ([[Curses your target, decreasing all its resistances by %d%%.
+		The resistances will decrease with Magic stat.]]):format(self:combatTalentSpellDamage(t, 10, 60))
+	end,
+}
