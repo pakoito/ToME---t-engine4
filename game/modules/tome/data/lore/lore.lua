933438f5f2461c166bfced7b83270f91fd377644
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-09-14 21:53:01 +0000

    lore
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1209 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/lore/lore.lua
+++ game/modules/tome/data/lore/lore.lua
@@ -17,6 +17,10 @@
 -- Nicolas Casalini "DarkGod"
 -- darkgod@te4.org
 
+--------------------------------------------------------------------------
+-- Trollshaws
+--------------------------------------------------------------------------
+
 newLore{
 	id = "trollshaws-note-1",
 	category = "trollshaws",
@@ -56,4 +60,54 @@ newLore{
 	name = "tattered paper scrap (trollshaws)",
 	lore = [[You find a tattered page scrap. Perhaps this is part of a diary entry.
 "...writing this in a tree and he's at the bottom of it. Waiting. His club is the size of a tall dwarf. Don't think I'm going to make it."]],
+}
+
+--------------------------------------------------------------------------
+-- Tol Falas
+--------------------------------------------------------------------------
+newLore{
+	id = "tol-falas-note-1",
+	category = "tol falas",
+	name = "note from the Master",
+	lore = [[MINIONS: Perhaps you feel your Master has been lax or absent? Well, I shall amend that. I have been studying an object of great import. It is of much greater interest than your foolish unlives. But do not think that I will let you get away with things because of this.
+
+Skeletons, you have been getting noticeably behind in your adventurer slaughtering quotas. The next skeleton archer I see drinking coffee and chatting with the wights shall be rended limb from limb and fed to the orcs. Also, as a punishment for your general laxness, 1,000 skeletons shall be remanded down to Amun Sul as punishment. A further 250 shall be slaughtered. These orders to be carried out by myself tomorrow at 3am.]],
+}
+
+newLore{
+	id = "tol-falas-note-2",
+	category = "tol falas",
+	name = "note from the Master",
+	lore = [[MINIONS: Be aware, I your great Master have found an item. It is of extreme power, but not yet complete, at least for my purposes.
+
+All hail your brilliant Master. Would you like to walk in the sun? Would you like to be free to roam green meadows and crush innocent children? Such are my wishes also. The reward to anyone who brings me any item that will help me bend this sta... item to my will shall be stupendous.
+
+Also, any new minions who have magical research skills are wanted. Recruit them and you shall be rewarded. Though if they steal my secrets, your blood will be my wine and your heart my appetizer.]],
+}
+
+newLore{
+	id = "tol-falas-note-3",
+	category = "tol falas",
+	name = "note from the Master",
+	lore = [[MINIONS: Perhaps you are minor dens of foulness because you have nothing to aspire to? Perhaps you could be greater if you had a worse example before you? Consider Me! I began my long unlife as a foolish pipsqueak such as yourself. Why, there was a time before I had conquered even a pit let alone a level or a dungeon. Now, behold all that is mine.
+
+You must have aspirations. I am not content with just the rule of Tol Falas. No, soon I shall have more. Much more. My boots shall tread the surface of the earth! I shall explore and destroy the most beautiful mountains. All shall be mine once I can walk in the sun once more. Where will you be? Do you wish to be more than the wight I stepped on yesterday? I shall need great leaders to guide my armies across the land.]],
+}
+
+newLore{
+	id = "tol-falas-note-4",
+	category = "tol falas",
+	name = "note from the Master",
+	lore = [[MINIONS: You are foolish sods. Have you heard of the great Sauron? Perhaps not because you know very little and he died long before you were so stupid as to be born. However, he was great and had the chance to be greater still. Sauron let himself be cooped into a small hellhole and was destroyed when the right hero invaded it and destroyed his plaything.
+
+I shall not make this mistake. Be ready for soon we shall march out upon the lands. Soon we shall conquer as even the great Sauron could not. Pity the fool who thinks he can keep my stuck in even the grandest of pits, Tol Falas.]],
+}
+
+newLore{
+	id = "tol-falas-note-5",
+	category = "tol falas",
+	name = "note from the Master",
+	lore = [[MINIONS: To my newest vampire, burn foolish adventurer, burn! I bet you are sorry for that flame spell now, aren't you? Suffer as I revisit it upon you.
+
+To the rest of you, there will be punishment. An adventurer got down to my bedroom and surprised me. I, myself, was hurt and almost had to use my special power. All is well now and I am as dangerous as ever, but you shall suffer for letting him get so low. The next minion I see shall be toasted with my marshmellows. Where then were the special pits of doom I organized? Where was the poison of my wights or the diseases of my ghouls? Indeed, I should slaughter all of you and I would, but those who were most foully remiss were already slaughtered by the adventurer. The rest of you? Beware my wrath.]],
 }
