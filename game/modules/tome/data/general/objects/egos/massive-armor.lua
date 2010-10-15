619d41bf4ac1b1c647abb7c77f3564d6ea28a329
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-10-15 19:11:01 +0000

    azd
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1536 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/general/objects/egos/massive-armor.lua
@@ -0,0 +1,47 @@
+-- ToME - Tales of Middle-Earth
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+local Talents = require("engine.interface.ActorTalents")
+local Stats = require "engine.interface.ActorStats"
+
+load("/data/general/objects/egos/armor.lua")
+
+newEntity{
+	name = " of the mumakil", suffix=true, instant_resolve=true,
+	level_range = {20, 50},
+	rarity = 20,
+	cost = 60,
+	wielder = {
+		resists={
+			[DamageType.ACID] = resolvers.mbonus_material(10, 5, function(e, v) return v * 0.15 end),
+			[DamageType.LIGHTNING] = resolvers.mbonus_material(10, 5, function(e, v) return v * 0.15 end),
+			[DamageType.FIRE] = resolvers.mbonus_material(10, 5, function(e, v) return v * 0.15 end),
+			[DamageType.COLD] = resolvers.mbonus_material(10, 5, function(e, v) return v * 0.15 end),
+			[DamageType.PHYSICAL] = resolvers.mbonus_material(10, 5, function(e, v) return v * 0.15 end),
+	},
+		inc_stats = {
+			[Stats.STAT_CON] = resolvers.mbonus_material(5, 1, function(e, v) return v * 3 end),
+			[Stats.STAT_STR] = resolvers.mbonus_material(5, 1, function(e, v) return v * 3 end),
+		},
+		stun_immune = 0.3,
+		knockback_immune = 0.3,
+		disarm_immune=0.3,
+		talent_cd_reduction={[Talents.T_RUSH]=10},
+	},
+}
