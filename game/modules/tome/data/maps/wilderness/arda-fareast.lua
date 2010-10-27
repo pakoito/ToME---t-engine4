8388b891f7e52b5926fb2fff350b6b86a85b98fd
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-10-27 19:39:04 +0000

    New far east map
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1638 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/maps/wilderness/arda-fareast.lua
+++ game/modules/tome/data/maps/wilderness/arda-fareast.lua
@@ -28,6 +28,8 @@ quickEntity('t', {always_remember = true, show_tooltip=true, name='forest', disp
 quickEntity('p', {always_remember = true, show_tooltip=true, name='oasis', display='#', color=colors.LIGHT_GREEN, back_color={r=163,g=149,b=42}, image="terrain/sand.png", add_displays = {mod.class.Grid.new{image="terrain/palmtree_alpha1.png"}}, block_move=true})
 quickEntity('m', {always_remember = true, show_tooltip=true, name='mountains', display='^', color=colors.LIGHT_UMBER, back_color=colors.UMBER, image="terrain/mountain.png", block_move=true})
 quickEntity('h', {always_remember = true, show_tooltip=true, name='low hills', display='^', color=colors.GREEN, back_color=colors.DARK_GREEN, image="terrain/hills.png", can_encounter="plain", equilibrium_level=-10})
+quickEntity('.', {always_remember = true, show_tooltip=true, name='road', display='.', color=colors.WHITE, back_color=colors.DARK_GREEN, image="terrain/marble_floor.png", can_encounter="plain", equilibrium_level=-10})
+
 
 quickEntity('A', {always_remember = true, show_tooltip=true, name="Sun Wall Outpost (Town)", display='*', color=colors.GOLD, notice = true, change_level=1, change_zone="town-sunwall-outpost"})
 quickEntity('B', {always_remember = true, show_tooltip=true, name="High Peak", display='>', color=colors.VIOLET, notice = true, change_level=1, change_zone="high-peak"})
@@ -51,60 +53,60 @@ addData{ encounters = {
 }
 
 return [[
-=================================================================================
-=================================================================================
-===========       ========================    ===================================
-========             ===================       ==================================
-=======               ================          =================================
-=====                 ================              =============================
-====                  ===============                 ===========================
-===                    ==========               hh    ===========================
-==t  tttt           =============           hhhhhh          =====================
-==t ttttt         ~==  ==========           hhhhh                ================
-==ttttttt         ~    ===========                              =================
-==ttttttt        ~~    ==========~~~                            =================
-==ttttttt       ~~     ===  ===    ~~~                         ==================
-==ttttt         ~       =            ~~~~~~                   ===================
-===tt           ~                         ~~~                   =================
-====            ~~~                                  tt            ==============
-=====             ~~~~                             ttt              =============
-======               ~                           tttt                  ==========
-=======              ~~                       tttttt                    =========
-========              ~~                     tttttt                      ========
-=========     hhh      ~~ tt       ttttttt   tttttt             hh        =======
-==========   hhhh       ~~ tttttttttttttmmmmmtttttt           hhh         =======
-==========   hhhh        ~~ttttttttttttmmmmmmmmttttt        hhhhh         =======
-=========    hhh          ~~ttttttttttmmmmmmmmmmtttt        hhh           =======
-=========                  ~ttttttttttmmmmmmmmmmttt                      ========
-========                ~~~~~~~~tttttt~~mmmBmmmmttt                     =========
-=======              ~~~~      ~~~~~~~~ttmmmmmmmtttt                   ==========
-======            ~~~~             tttttttmmmmmmtttt                   ==========
-=====           ~~~                ttttttmmmmmmmmttt                  ===========
-=====        ~~~~                    ttttmmmmtmmmmtt               w  ===========
-====~~~~~~~~~~                         mmmmtttttmmt               www============
-=====                                  mmtttttttt                 www============
-=====                                                             www============
-=====                                                             www============
-======                                                            www============
-========          =======              hhhhhh                     1ww============
-==============================       hhhhhhh                      www============
-================================         h                        www============
-=================================                  hh             www============
-========== ======================                  hhh            www============
-=======h   ======================tttt             hhhh            www============
-=====hh     ====================tttttttt          hh               w  ===========
-======mm    ====================tttttttttt                            ===========
-======tttmm A===================ttttttttttt                            ==========
-======ttt=mm===================stttttttttttt                sssssss    ==========
-=======t======================sspssstttttttt            sssssssssssssss==========
-=============================sspssppssp            ssssssssssssssssssss==========
-=============================ssssssssssssssssssssssssssssssssssssssssss==========
-=============================sssssssssssssssssssssssssssssssssssssssss===========
-=============================ssssssssssssssssssssspssssssssssssspssss============
-=============================sssssssssssssssmmmsssssssssssssssssssss=============
-==============================sssssssssspssmmmmmsssssssssssssssssss==============
-==============================ssssssssssssmmssssssssssspssssssssss===============
-================================ssssssssssmmmmmmsssssssssssssssss================
-===================================ssssssmmmmmmmmssssss==========================
-============================================mmmmsss==============================
-=================================================================================]]
+mmmttttttttttttttttttttttttttttttttt~tttttttttttttttttttttttttttttttttttttttttmmm
+m mmmttttttttttttttt  thhhttttttt ~~~tttttt           t  ttttttttt    ttt     mmm
+m mmmmmmmhh    tttt tthhhhttttt ~~~ttttt     mmmmmm    tt tttt    tttt t ttttmmmm
+mm  mmmhh     tttttt thhhhhtttt ~tttttt       mmmmmmmmtttt    ttttttttt ttttmmmmm
+mmmm hh        ttttt ttttttttt ~~ttttt           mmmmmmmtttttt ttttttmmmmmmmmmmmm
+hhhhh             t ttttt      ~                    mmmmmmmtttt tttmmmmmmmmmmmmmm
+tttt                          ~~                      mmmmmmmmtt ttmmmmmmhhmmmmmm
+ttt                      =====~                 hh         mmmmt tmmmmmmhtthmmmmm
+ttt  tttt           ~~~~=tt=====            hhhhhh          mmmmmmmmmmmhttthmmmmm
+ttt ttttt         ~~~  = h ======           hhhhh                mmmmhhttt  hmmmm
+ttttttttt         ~    == ========.........................     mmmmhttt   thmmmm
+ttttttttt        ~~    ==========~~~                      .     mmmhttt    tthmmm
+ttttttttt       ~~      ===  ==    ~~~                    .    mmmmhttt   ttthmmm
+ttttttt         ~                    ~~~~~~               .   mmmmmmhtttttttthmmm
+=tttt           ~                         ~~~     m       .   mmmmhhtttttttttthmm
+===             ~~~                              mm  tt   .........   tttttttthmm
+=====             ~~~~                          mmmttt    .   hhh .     ttttthmmm
+======               ~                        mmmmttt     .    hh .      tttthmmm
+=======              ~~                     mmmmtttt      .   hhh .        hhmmmm
+========              ~~                  mmmmttttt       .........       hmmmmmm
+=========     hhh      ~~ tt       tttttttmmmtttttt........hhhhhhh.      hmmmmmmm
+==========   hhhh       ~~ tttttttttttttmmmmmtttttthhhh   hmmmhhh..      hmmmmmmm
+==========   hhhh        ~~ttttttttttttmmmmmmmmtmmmmmmmmhhmmhhhhh.       hmmmmmmm
+=========    hhh          ~~ttttttttttmmmmmmmmmmmmmhhhmmmmmmhhh...       hmmmmmmm
+=========                  ~ttttttttttmmmmmmmmmmttt     mmm.....         mmmmmmmm
+========                ~~~~~~~~tttttt~~mmmBmmmmttt ........            mmmmmmmmm
+=======              ~~~~      ~~~~~~~~ttmmmmmmmtttt.                  mmmmmmmmmm
+======            ~~~~             tttttttmmmmmmtttt.                  mmmmmmmmmm
+=====           ~~~                ttttttmmmmmmmmttt.                 wwhhhhhhhhm
+=====        ~~~~                    ttttmmmmtmmmh...              w  wwwhhhhhhmm
+====~~~~~~~~~~                         mmmmtttttmhthmmm           wwwwwwwwhhhmmmm
+=====                                  mmtttttttthmmmh           hwwwwwwwwhhmmmmm
+=====                                 mmm.........mm             hwwwwwww  hmmmmm
+=====                                 mm..       .               hwwwwwww  hmmmmm
+======    ...........................mm..        .               hwwwwww   hmmmmm
+========  .       ==               hhhhhhhhhh.....................1wwwwhh   hmmmm
+=====================              mhhhhhhhh     .               hwwwwwhh   hmmmm
+======================            mm.     h      .               hwwwwhh    hmmmm
+======================          mmm..            . hh             wwwwh      hmmm
+========== ==========         hmmmm.             . hhh            hwwh       hmmm
+=======h   ==========        hmmmtt.t            .hhhh            hww        hmmm
+=====hh     ==========      hmmmttt.tttt         .hh               wh         hmm
+======mm    ===========    hhmmmttt.tttttt       .                            hmm
+======tttmm A==========   hmmmmmttt.ttttttt      .                             hm
+======ttt=mm=========     hmmmmsttt.tttttttt     .          sssssss             m
+=======t============    hhmmmmsspss.tttttttt     .      sssssssssssssssssssssssmm
+=================tttttthmmmmmsspssp.ssp         .. ssssssssssssssssssssssssssmmmm
+===============tttttttthmm mmssssss.ssssssssssss.sssssssssssssssssssssssssmmmmmmm
+==============tttttttt ttmhmmssssss.ssssssssssss.sssssssssssssssssss.....mmmmmmmm
+==========  tttttttttt ttmhmmssssss...............p.............p....mmmhmmmmmmmm
+========       tttttttt thmmmsssssssssssssssmmms.sssssssssssssssssssmmmmhmmmmmmmm
+======= ttttttt tttttt t tmmmmsssssssssspssmmmmm.ssssssssssssssssssmmmmhmm   mmmm
+=====  ttttttttt tttt ttthmmmmssssssssssssmm.....sssssspssssssssssmmmmhmm   mmmmm
+==   ttttt  tttt tttt ttmmhh mmmssssssssssmmmmmmsssssssssssssssssmmmmmhmm   mmmmm
+=   t       ttttt tt tttmmmmmmmmmmmssssssmmmmmmmmssssssmmmmmmmmmmmmmmmmhm  mmmmmm
+=   tttttttttttttt  tttmmmmmmmmmmmmmmmmmmmmmmmmmsssmmmmmmmmmmmmmmmmmmmmmhh mmmmmm
+ttttttttttttttttttttttmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm]]
