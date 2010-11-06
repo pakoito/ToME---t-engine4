03abf777e2edf1a23d8c82194ebe45a023734b03
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-11-06 15:53:10 +0000

    New achievements for going deep in the infinite dungeon
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1796 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/achievements/infinite-dungeon.lua
@@ -0,0 +1,79 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+newAchievement{
+	name = "Infinite x10",
+	desc = [[Got to the level 10 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x20",
+	desc = [[Got to the level 20 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x30",
+	desc = [[Got to the level 30 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x40",
+	desc = [[Got to the level 40 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x50",
+	desc = [[Got to the level 50 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x60",
+	desc = [[Got to the level 60 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x70",
+	desc = [[Got to the level 70 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x80",
+	desc = [[Got to the level 80 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x90",
+	desc = [[Got to the level 90 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x100",
+	desc = [[Got to the level 100 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x150",
+	desc = [[Got to the level 150 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x200",
+	desc = [[Got to the level 200 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x300",
+	desc = [[Got to the level 300 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x400",
+	desc = [[Got to the level 400 of the infinite dungeon.]],
+}
+newAchievement{
+	name = "Infinite x500",
+	desc = [[Got to the level 500 of the infinite dungeon.]],
+}
--- game/modules/tome/data/zones/infinite-dungeon/zone.lua
+++ game/modules/tome/data/zones/infinite-dungeon/zone.lua
@@ -22,7 +22,7 @@ return {
 	level_range = {1, 1},
 	level_scheme = "player",
 	max_level = 1000000000,
-	actor_adjust_level = function(zone, level, e) return math.floor((zone.base_level + level.level-1) * 1.7) + e:getRankLevelAdjust() + rng.range(-1,2) end,
+	actor_adjust_level = function(zone, level, e) return math.floor((zone.base_level + level.level-1) * 1.2) + e:getRankLevelAdjust() + rng.range(-1,2) end,
 	width = 70, height = 70,
 --	all_remembered = true,
 --	all_lited = true,
@@ -57,5 +57,23 @@ return {
 	post_process = function(level)
 		-- Everything hates you in the infinite dungeon!
 		for uid, e in pairs(level.entities) do e.faction="enemies" end
+
+		-- Provide some achievements
+		if level.level == 10 then world:gainAchievement("INFINITE_X10", game.player)
+		elseif level.level == 20 then world:gainAchievement("INFINITE_X20", game.player)
+		elseif level.level == 30 then world:gainAchievement("INFINITE_X30", game.player)
+		elseif level.level == 40 then world:gainAchievement("INFINITE_X40", game.player)
+		elseif level.level == 50 then world:gainAchievement("INFINITE_X50", game.player)
+		elseif level.level == 60 then world:gainAchievement("INFINITE_X60", game.player)
+		elseif level.level == 70 then world:gainAchievement("INFINITE_X70", game.player)
+		elseif level.level == 80 then world:gainAchievement("INFINITE_X80", game.player)
+		elseif level.level == 90 then world:gainAchievement("INFINITE_X90", game.player)
+		elseif level.level == 100 then world:gainAchievement("INFINITE_X100", game.player)
+		elseif level.level == 150 then world:gainAchievement("INFINITE_X150", game.player)
+		elseif level.level == 200 then world:gainAchievement("INFINITE_X200", game.player)
+		elseif level.level == 300 then world:gainAchievement("INFINITE_X300", game.player)
+		elseif level.level == 400 then world:gainAchievement("INFINITE_X400", game.player)
+		elseif level.level == 500 then world:gainAchievement("INFINITE_X500", game.player)
+		end
 	end,
 }
