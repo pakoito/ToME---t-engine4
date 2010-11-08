2926ea60b5df6564369f5c765a98a1453f1dff51
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-11-08 16:44:23 +0000

    Archers/Slingers can not start with ego ammo, to prevent problems
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1827 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/maps/vaults/mold-path-dark.lua
@@ -0,0 +1,50 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+setStatusAll{no_teleport=true}
+
+rotates = {"default", "90", "180", "270", "flipx", "flipy"}
+
+defineTile('.', "GRASS_DARK1")
+defineTile('#', "TREE_DARK")
+defineTile('X', "HARDTREE_DARK")
+defineTile('~', "POISON_SHALLOW_WATER")
+defineTile('!', "ROCK_VAULT")
+
+defineTile('m', nil, {random_filter={subtype="molds", add_levels=2}})
+defineTile('j', nil, {random_filter={subtype="oozes", add_levels=2}})
+
+defineTile('$', {random_filter={add_levels=5, ego_chance=25}})
+
+startx = 19
+starty = 7
+
+return {
+[[#XXXXXXXXX#####XXXX#]],
+[[XXm~..~.~XXXX#XX$$XX]],
+[[X~~...j..~~jXXXmmmmX]],
+[[X~...mXX.j...j..jjXX]],
+[[Xm..~XXXXX~~.~.jXXX#]],
+[[XX.mmXXXXXXXXXXXX###]],
+[[XmmmXXXXXXXXXXXXXXXX]],
+[[X~..mXXXX~.mmXXm...!]],
+[[XXm...m.........~mXX]],
+[[#XXXX~~.mXXXm~m~XXX#]],
+[[####XXXXXX#XXXXXX###]],
+}
