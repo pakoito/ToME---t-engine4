984585b5e9676e95cc5697a646abaa1e30f1aaa1
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-11-20 02:30:24 +0000

    Prevent empathic hexe/martydom/... on two hostile targets to "ricochet" forever
    git-svn-id: http://svn.net-core.org/repos/t-engine4@1965 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/damage_types.lua
+++ game/modules/tome/data/damage_types.lua
@@ -18,7 +18,7 @@
 -- darkgod@te4.org
 
 -- The basic stuff used to damage a grid
-setDefaultProjector(function(src, x, y, type, dam)
+setDefaultProjector(function(src, x, y, type, dam, no_martyr)
 	local target = game.level.map(x, y, Map.ACTOR)
 	if target then
 		local rsrc = src.resolveSource and src:resolveSource() or src
@@ -107,8 +107,8 @@ setDefaultProjector(function(src, x, y, type, dam)
 			end
 		end
 
-		if src.attr and src:attr("martyrdom") then
-			DamageType.defaultProjector(target, src.x, src.y, type, dam * src.martyrdom / 100)
+		if src.attr and src:attr("martyrdom") and not no_martyr then
+			DamageType.defaultProjector(target, src.x, src.y, type, dam * src.martyrdom / 100, true)
 		end
 
 		if target.knowTalent and target:knowTalent(target.T_RESOLVE) then local t = target:getTalentFromId(target.T_RESOLVE) t.on_absorb(target, t, type, dam) end
