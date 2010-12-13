fac93b0c1bedf1f6baaa6e2c18ef73956c11f54a
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-12-13 23:53:11 +0000

    High level Phase Door & Teleport will indicate which targetting does what
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2133 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/spells/conveyance.lua
+++ game/modules/tome/data/talents/spells/conveyance.lua
@@ -34,6 +34,7 @@ newTalent{
 	action = function(self, t)
 		local target = self
 		if self:getTalentLevel(t) >= 4 then
+			game.logPlayer(self, "Selects a target to teleport...")
 			local tg = {default_target=self, type="hit", nowarning=true, range=10, first_target="friend"}
 			local tx, ty = self:getTarget(tg)
 			if tx then
@@ -58,6 +59,7 @@ newTalent{
 		local rad = t.getRange(self, t)
 		local radius = t.getRadius(self, t)
 		if self:getTalentLevel(t) >= 5 then
+			game.logPlayer(self, "Selects a teleport location...")
 			local tg = {type="ball", nolock=true, pass_terrain=true, nowarning=true, range=rad, radius=radius, requires_knowledge=false}
 			x, y = self:getTarget(tg)
 			if not x then return nil end
@@ -112,6 +114,7 @@ newTalent{
 		local target = self
 
 		if self:getTalentLevel(t) >= 5 then
+			game.logPlayer(self, "Selects a target to teleport...")
 			local tg = {default_target=self, type="hit", nowarning=true, range=10, first_target="friend"}
 			local tx, ty = self:getTarget(tg)
 			if tx then
@@ -135,6 +138,7 @@ newTalent{
 
 		local x, y = self.x, self.y
 		if self:getTalentLevel(t) >= 4 then
+			game.logPlayer(self, "Selects a teleport location...")
 			local tg = {type="ball", nolock=true, pass_terrain=true, nowarning=true, range=t.getRange(self, t), radius=t.getRadius(self, t), requires_knowledge=false}
 			x, y = self:getTarget(tg)
 			if not x then return nil end
