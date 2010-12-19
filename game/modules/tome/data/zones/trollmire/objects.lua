b35620a8c63ae3fe382ddd45d54f5cb48dac99e3
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-12-19 16:33:22 +0000

    typos
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2168 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/zones/trollmire/objects.lua
+++ game/modules/tome/data/zones/trollmire/objects.lua
@@ -44,7 +44,7 @@ This seems to be what the troll was keeping as "treasure". You have heard of suc
 		use = function(self, who)
 			if who:canBe("worldport") then
 				who:setEffect(who.EFF_RECALL, 40, {})
-				game.logPlayer(who, "Space around you starts to disolve...")
+				game.logPlayer(who, "Space around you starts to dissolve...")
 			else
 				game.logPlayer(who, "The rod emits a strange noise, glows briefly and returns to normal.")
 			end
