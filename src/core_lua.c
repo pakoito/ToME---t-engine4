9459850ad98a14b9e27aae343f0b8891d58ee03f
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2010-12-28 14:31:08 +0000

    plop
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2213 51575b47-30f0-44d4-a5cc-537603b46e54

--- src/core_lua.c
+++ src/core_lua.c
@@ -2206,7 +2206,7 @@ int luaopen_core(lua_State *L)
 
 	luaL_openlib(L, "core.game", gamelib, 0);
 	lua_pushstring(L, "VERSION");
-	lua_pushnumber(L, 9);
+	lua_pushnumber(L, 10);
 	lua_settable(L, -3);
 
 	luaL_openlib(L, "rng", rnglib, 0);
