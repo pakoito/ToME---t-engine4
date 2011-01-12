ead62cd1830662108011fe9f50ae5464de7d6cfb
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-01-12 18:16:44 +0000

    plop
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2359 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/lore/age-allure.lua
@@ -0,0 +1,135 @@
+f-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+--------------------------------------------------------------------------
+-- Temple of Creation
+--------------------------------------------------------------------------
+
+newLore{
+	id = "halfling-research-note-1",
+	category = "age of allure",
+	name = "research log of halfling mage Hompalan",
+	lore = [[
+#{bold}#Hompalan's Log Entry1#{normal}#
+#{italic}#Age of Allure Year 4537#{normal}#
+
+Excavation of Sher'Tul ruins almost complete. Markings seem to match previous ruins in the south.
+Current theory: Possibly a portal of some kind? We have only rumours and myths of the Sher'Tul, and all say they could travel Eyal in the blink of an eye. Mana readings of the place are beyond anything we can even begin to comprehend. Last diviner I sent down there is still in a coma. Must continue studies.
+
+
+#{bold}#Hompalan's Log Entry 2#{normal}#
+#{italic}#Age of Allure Year 4541#{normal}#
+
+Testing of live animals has not gone well. Thankfully I can leave the clean-up to my technicians... Four years and it feels like we have not even begun to understand these ruins. I'm worried our research grant will soon be cut. But I have hope that some of the items we've recently uncovered may provide some sort of key to the portals. Or perhaps they were just used for Sher'Tul sports? We shall see.
+
+
+#{bold}#Hompalan's Log Entry 3#{normal}#
+#{italic}#Age of Allure 4542#{normal}#
+
+Third test confirms it - the orb allows transition between portal gates. This is the greatest discovery of my life. We are the first research team to ever unlock such powers from a Sher'Tul ruin! Just a shame about all the animals we sent through before. I'm really starting to suspect that pronged instrument was some sort of eating device... Oh well, plenty more test animals available.
+]]
+
+newLore{
+	id = "halfling-research-note-2",
+	category = "age of allure",
+	name = "research log of halfling mage Hompalan",
+	lore = [[
+#{bold}#Hompalan's Log Entry 4#{normal}#
+#{italic}#Age of Allure 4544#{normal}#
+
+Just when I was getting somewhere the military decide to barge in and take over. Don't they realise what a serious scientific project this is? All they care about is the little stripes on their helmets. Some tell rumours of humans threatening to attack the facility, but I really care not for such trivial politics. Besides, what threat could those stupid lanklegs really be? I suppose I shall have to persevere though, and try to work around these insane security restrictions.
+
+
+#{bold}#Hompalan's Log Entry 5#{normal}#
+#{italic}#Age of Allure 4545#{normal}#
+
+Well, I suppose this whole war thing wasn't just hot wind after all. Apparently there's lots of people dying outside. What a nuisance - I just want to get on with my work without my supplies being cut off. There is one upside though - human test subjects! We're going to get started on them in the coming months.
+
+
+#{bold}#Hompalan's Log Entry 6#{normal}#
+#{italic}#Age of Allure 4546#{normal}#
+
+Test subject A-C: Imploded during transition.
+Test subject D: Exploded during transition.
+Test subject E: Half transitioned, half remained. Partial success?
+Test subject F: Imploded during transition.
+Test subject G: Turned to goo during transition.
+Test subject H-K: Imploded during transition.
+Test subject L: Frozen during transition.
+Test subject M: Survived first transition. Imploded 2 seconds after. Progress!
+Test subject N: Survived first transition, but in coma. Died after 4 days.
+
+We're really getting somewhere here... Just a shame humans are such messy creatures! Honestly, how much intestines do they need?! Will start on the next set of subjects early in the new year.
+]]
+
+newLore{
+	id = "halfling-research-note-3",
+	category = "age of allure",
+	name = "research log of halfling mage Hompalan",
+	lore = [[
+#{bold}#Hompalan's Log Entry 7#{normal}#
+#{italic}#Age of Allure 4547#{normal}#
+
+Test subject O: Imploded during transition.
+Test subject P: Survived first transition, but turned mad - had to be put down.
+Test subject Q: Survived first transition. Imploded on return transition.
+Test subject R: Died during first transition.
+Test subject S-T: Imploded on return transition.
+Test subject U: Survived return transition. Muttered something about hearing a voice before jumping back into portal - imploded. What a nuisance!
+Test subject V: Died on return transition.
+
+Running out of letters soon. Also out of subjects for now. Will have to wait for the soldiers to fetch me more.
+
+
+#{bold}#Hompalan's Log Entry 8#{normal}#
+#{italic}#Age of Allure 4548#{normal}#
+
+Test subject W: Shrunk during first transition, before exploding. (error in calibration?)
+Test subject X: Returned from second transition missing head. How bizarre.
+Test subject Y: Disappeared during transition.
+Test subject Z: Survived both transitions. Remarkable!
+
+Subject Z currently raving, but I believe this is due to stressful conditions, not a direct corrosion of mental faculties from the portal use. Will have to study further.
+]]
+
+newLore{
+	id = "halfling-research-note-4",
+	category = "age of allure",
+	name = "research log of halfling mage Hompalan",
+	lore = [[
+#{bold}#Hompalan's Log Entry 9#{normal}#
+#{italic}#Age of Allure 4549#{normal}#
+
+Subject Z is still doing well. Have completed numerous further transitions with him. Is capable of intelligent dialogue at times, though his species is fairly limited in intellect. Seems to have little real clue as to what is going on, and is always asking what time it is. What a fool... I must conjecture that humans are far too tall to have blood pumped all the way to their brain. Still, I dare not risk sending any halflings through the portal yet, and what humans I have sent through since have all died.
+
+
+#{bold}#Hompalan's Log Entry 10#{normal}#
+#{italic}#Age of Allure 4550#{normal}#
+
+Beginning to suspect subject Z has latent magical powers. Have seen him move much faster than I thought his species capable, and mend broken objects without any obvious means of repair. These are small things, but I believe the portals may be somehow enhancing a basic ability. Will need to test more. He still seems quite dumb though. Said his last journey took three days, when clearly it took but seconds. He began to eat ravenously afterwards. I suspect he is just greedy. I saw him staring for a long time at the farportal earlier, but I had my assistants pull him away before he did any damage.
+
+
+#{bold}#Hompalan's Log Entry 11#{normal}#
+#{italic}#Age of Allure 4551#{normal}#
+
+Subject Z has vanished! This is terrible! It was not even during a portal transit. It looked like he simply stepped into his own shadow and disappeared. My research is ruined!! There will never be another subject like him!
+
+The military are annoying me to no end. I told them to find me more test subjects immediately, but they gave some excuses about enemy patrols in the area. Do they think I care about such trivial things?! And now I hear them practising their fighting in the corridors. They are even grunting and screaming in fake battle noises like stupid children. Do they not realise what an important facility this is? Can they not understand how my genius is disturbed by---
+]],
+}
