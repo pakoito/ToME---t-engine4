eb712db53b028511b5eeeaf5dab2aaf49257d23c
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-01-28 19:01:29 +0000

    Resting in water will not suffocate you to death
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2524 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/class/Player.lua
+++ game/modules/tome/class/Player.lua
@@ -429,7 +429,7 @@ end
 --- Suffocate a bit, lose air
 function _M:suffocate(value, src)
 	local dead, affected = mod.class.Actor.suffocate(self, value, src)
-	if affected and value < 0 then
+	if affected and value > 0 then
 		self:runStop("suffocating")
 		self:restStop("suffocating")
 	end
