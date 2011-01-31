d11e14f4ea3530775f0661be29919f57c23c71ff
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-01-31 13:20:44 +0000

    New town: Elvala, the Shaloren home city
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2581 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/maps/towns/elvala.lua
@@ -0,0 +1,93 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+startx = 0
+starty = 40
+endx = 0
+endy = 40
+
+-- defineTile section
+defineTile("#", "HARDWALL")
+defineTile("~", "DEEP_WATER")
+defineTile("<", "GRASS_UP_WILDERNESS")
+defineTile("_", "ROAD")
+defineTile(".", "GRASS")
+defineTile("t", {"TREE","TREE2","TREE3","TREE4","TREE5","TREE6","TREE7","TREE8","TREE9","TREE10","TREE11","TREE12","TREE13","TREE14","TREE15","TREE16","TREE17","TREE18","TREE19","TREE20"})
+
+quickEntity('2', {show_tooltip=true, name="Armour Smith", display='2', color=colors.UMBER, resolvers.store("ARMOR"), image="terrain/wood_store_armor.png"})
+quickEntity('3', {show_tooltip=true, name="Weapon Smith", display='3', color=colors.UMBER, resolvers.store("WEAPON"), image="terrain/wood_store_weapon.png"})
+quickEntity('4', {show_tooltip=true, name="Alchemist", display='4', color=colors.LIGHT_BLUE, resolvers.store("POTION"), image="terrain/wood_store_potion.png"})
+quickEntity('5', {show_tooltip=true, name="Scribe", display='5', color=colors.WHITE, resolvers.store("SCROLL"), image="terrain/wood_store_book.png"})
+
+-- addSpot section
+
+-- addZone section
+
+-- ASCII map section
+return [[
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~###~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~##_##~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~##___##~~~~~~~~t~~~~~~~~~~~~~
+~~~~~~~~~~~t~~~~~~~~~##___##~~~~~~~~~~~~~~~~~~~~~~
+~~~~~t~~~~~t~~~~~~~~###___###~~~~~~~~~~~~~t~~~~~~~
+~~~~~~~~~~~~~~~~~~~~###___###~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~t~~~####___####~~~t~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~####___####~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~t~~~~~~~~~####___####~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~t~~~~~~~~##2##___##3##~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~#___________#..~~~......~~~t~~~~
+~~~~~~~~~~~~~~~~~~#___________#............~~~~~~~
+~~~~~~~~~~~~~~~~~~#___________#.....tttt...~~~~~~~
+~~~~........~~~~~~#___________#.....ttttt.~~~~~~~~
+~~~............~~~#___________#......ttttt~~~~~~~~
+~~...............##___________##......ttt..~~~~~~~
+~~....ttt........#__#########__#...........~~~~~~~
+~....ttt.........#_###########_#...........~~~~~~~
+~...ttt.....######__#########__######.......~~~~~~
+~...t....####_______________________####......~~~~
+~......###_______#_____..._____#_______###.....~~~
+~.....##________###___.....___###________##....~~~
+~....##_________###__..ttt..__###_________##...~~~
+~....___________##5__..ttt..__4##___________...~~~
+~....##_________###__..ttt..__###_________##...~~~
+~~....##________###___.....___###________##.....~~
+~~.....###_______#_____..._____#_______###......~~
+~~.......####_______________________####........~~
+~~..........######__#########__######..........~~~
+~~...............#_###########_#...............~~~
+~.....t.tt.......#__#########__#...............~~~
+~....ttttt.......##___________##..............~~~~
+~....ttttt........#___________#......tt.......~~~~
+~~...tttt.....t...#___________#.....tttt.....~~~~~
+~~...tt.......t...#___________#....ttttt....~~~~~~
+~.............t...#___________#...tttttt....~~~~~~
+~.................#___________#...tttt......~~~~~~
+~..........tt.t...#####___#####..tttt.......~~~~~~
+.....tt....t.......####___####...ttt.......~~~~t~~
+<...ttt...t....tt..####___####.............~~~~~~~
+...tttt............####___####...........~~~~~~~~~
+~..tttt....~~.......###___###.........~~~~~~~~~~~~
+~..tttt....~~.......###___###........~~~~~~~~~~~~~
+~~........~~~~.......##___##........~~~~~~~~~~~~~~
+~~.......~~~~~~~.....##___##........~~~~~~~~~~~~~~
+~~~..~~~~~~~~~~~~.....##_##........~~~~~~~~~~t~~~~
+~~~~~~~~~~~~~~~~~~~~~..###.....~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~.......~~~~~~~~~~~t~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~]]
---
+++ game/modules/tome/data/zones/town-elvala/npcs.lua
@@ -0,0 +1,104 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+load("/data/general/npcs/gwelgoroth.lua", function(e) if e.rarity then e.derth_rarity, e.rarity = e.rarity, nil end end)
+
+local Talents = require("engine.interface.ActorTalents")
+
+newEntity{
+	define_as = "BASE_NPC_DERTH_TOWN",
+	type = "humanoid", subtype = "human",
+	display = "p", color=colors.WHITE,
+	faction = "allied-kingdoms",
+	anger_emote = "Catch @himher@!",
+
+	combat = { dam=resolvers.rngavg(1,2), atk=2, apr=0, dammod={str=0.4} },
+
+	body = { INVEN = 10, MAINHAND=1, OFFHAND=1, BODY=1, QUIVER=1 },
+	lite = 3,
+
+	life_rating = 10,
+	rank = 2,
+	size_category = 3,
+
+	open_door = true,
+
+	resolvers.inscriptions(1, "infusion"),
+
+	autolevel = "warrior",
+	ai = "dumb_talented_simple", ai_state = { ai_move="move_dmap", talent_in=3, },
+	energy = { mod=1 },
+	stats = { str=12, dex=8, mag=6, con=10 },
+}
+
+newEntity{ base = "BASE_NPC_DERTH_TOWN",
+	name = "derth guard", color=colors.LIGHT_UMBER,
+	desc = [[A stern-looking guard, he will not let you disturb the town.]],
+	level_range = {1, nil}, exp_worth = 1,
+	rarity = 3,
+	max_life = resolvers.rngavg(70,80),
+	resolvers.equip{
+		{type="weapon", subtype="longsword", autoreq=true},
+		{type="armor", subtype="shield", autoreq=true},
+	},
+	combat_armor = 2, combat_def = 0,
+	resolvers.talents{ [Talents.T_RUSH]=1, [Talents.T_PERFECT_STRIKE]=1, },
+}
+
+newEntity{ base = "BASE_NPC_DERTH_TOWN",
+	name = "halfling slinger", color=colors.UMBER,
+	subtype = "halfling",
+	desc = [[A Halfling, with a sling. Beware.]],
+	level_range = {1, nil}, exp_worth = 1,
+	rarity = 3,
+	max_life = resolvers.rngavg(50,60),
+	resolvers.talents{ [Talents.T_SHOOT]=1, },
+	ai_state = { talent_in=2, },
+	autolevel = "slinger",
+	resolvers.equip{ {type="weapon", subtype="sling", autoreq=true}, {type="ammo", subtype="shot", autoreq=true} },
+}
+
+newEntity{ base = "BASE_NPC_DERTH_TOWN",
+	name = "human farmer", color=colors.WHITE,
+	desc = [[A weather-worn Human farmer.]],
+	level_range = {1, nil}, exp_worth = 1,
+	rarity = 1,
+	max_life = resolvers.rngavg(30,40),
+	combat_armor = 2, combat_def = 0,
+}
+
+newEntity{ base = "BASE_NPC_DERTH_TOWN",
+	name = "halfling gardener", color=colors.WHITE,
+	subtype = "halfling",
+	desc = [[A Halfling, he seems to be looking for plants.]],
+	level_range = {1, nil}, exp_worth = 1,
+	rarity = 1,
+	max_life = resolvers.rngavg(30,40),
+}
+
+newEntity{ base = "BASE_NPC_DERTH_TOWN",
+	define_as ="ARENA_AGENT",
+	name = "Shady cornac man", color=colors.DARK_BLUE, unique = true,
+	level_range = {1, nil}, exp_worth = 1,
+	can_talk = "arena-unlock",
+	can_quest = true,
+	never_move = 1,
+	rarity = false,
+	max_life = resolvers.rngavg(70,80),
+}
