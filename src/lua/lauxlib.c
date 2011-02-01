9063d7eb5bc87b22ef907633c3eba5d5ad872c0c
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-02-01 17:37:09 +0000

    oups
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2592 51575b47-30f0-44d4-a5cc-537603b46e54

--- src/lua/lauxlib.c
+++ src/lua/lauxlib.c
@@ -620,8 +620,6 @@ static int panic (lua_State *L) {
   (void)L;  /* to avoid warnings */
   fprintf(stderr, "PANIC: unprotected error in call to Lua API (%s)\n",
                    lua_tostring(L, -1));
-	stackDump (L);
-  assert(0);
   return 0;
 }
 
