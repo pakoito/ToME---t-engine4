01b2199295b7f579744a36c47b62bf333ee15891
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-02-07 23:12:52 +0000

    Escort portals should be again visible in ascii mode
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2688 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/quests/escort-duty.lua
+++ game/modules/tome/data/quests/escort-duty.lua
@@ -347,7 +347,9 @@ on_grant = function(self, who)
 	g.show_tooltip = true
 	g.name = "Recall Portal: "..npc.name
 	g.display = '&'
-	g.color = colors.VIOLET
+	g.color_r = colors.VIOLET.r
+	g.color_g = colors.VIOLET.g
+	g.color_b = colors.VIOLET.b
 	g.add_displays = g.add_displays or {}
 	g.add_displays[#g.add_displays+1] = mod.class.Grid.new{image="terrain/maze_teleport.png"}
 	g.notice = true
@@ -360,6 +362,7 @@ on_grant = function(self, who)
 		who:removed()
 		game.party:removeMember(who, true)
 	end
+	g._mo = nil
 
 	g:resolve() g:resolve(nil, true)
 	game.zone:addEntity(game.level, g, "terrain", gx, gy)
