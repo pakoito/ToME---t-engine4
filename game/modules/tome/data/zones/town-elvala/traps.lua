884fec834eb25e678d92b7ae4ec1fb4396d70eca
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-02-10 01:08:38 +0000

    fix
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2705 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/zones/town-elvala/traps.lua
@@ -0,0 +1,38 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+load("/data/general/traps/store.lua")
+
+newEntity{ base = "BASE_STORE", define_as = "ARMOR_STORE",
+	name="Armoury",
+	display='2', color=colors.UMBER,
+	resolvers.store("ARMOR"),
+}
+
+newEntity{ base = "BASE_STORE", define_as = "WEAPON_STORE",
+	name="Weapon Store",
+	display='3', color=colors.UMBER,
+	resolvers.store("WEAPON"),
+}
+
+newEntity{ base = "BASE_STORE", define_as = "RUNEMASTER",
+	name="Runemaster",
+	display='5', color=colors.RED,
+	resolvers.store("SCROLL"),
+}
