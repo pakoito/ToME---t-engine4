75c23165afa4398e319c35726a5e544edc770cd6
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-02-24 01:21:25 +0000

    New lore: Spellhunter's guide
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2811 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/lore/spellhunt.lua
+++ game/modules/tome/data/lore/spellhunt.lua
@@ -17,7 +17,6 @@
 -- Nicolas Casalini "DarkGod"
 -- darkgod@te4.org
 
-
 newLore{
 	id = "spellhunt-note-1",
 	category = "age of dusk",
@@ -32,4 +31,70 @@ Other mages, those less forgiving and more given to action, would attack the rov
 Ones I saw were set upon by dozens, their conjured flames and bolts of energy failing against the maddened crowds... they would often literally rip the mage limb from limb. It was horrific.
 ...
 Angolwen, Linaniil calls it. I have known Linaniil for many years, and I know she has lost many loved ones to the Spellhunt, but she still holds true to her belief that one day we will be accepted once again. If this is the course of action she desires, I will follow her without question.]],
+}
+
+newLore{
+	id = "spellhunt-note-2",
+	category = "age of dusk",
+	name = "Spellhunter's Guide part 1: How to Detect a Spellweaver",
+	lore = [[Those who partake in the profane sorcery can oft be marked by their appearance, their mannerisms, their personal keeping and their effect on the environment around them. Keep close watch on all you meet, for they are deceptive creatures that can take on many guises. Even women, children and cripples may be clever disguises of demon-communers and necromancers.
+
+In their appearance you should be wary of the following:
+* Particular grossness in complexion
+* Unnatural or "elven" beauty
+* Extreme thinness, with prominent bones
+* Pointed or sharp-tipped ears - beware indeed of those with long hair or hats covering their ears
+* Hunched backs and distorting limps
+* Peculiar eye colour
+
+In their behaviour you should be wary of the following:
+* Tiredness during daylight hours and heavy activity at night
+* Social aversion and fear of crowded places
+* A questioning and probing attitude, in particular if challenging the precepts and set morals of society
+* Quick-wittedness, especially where used for guile
+* Furtiveness or a secretive attitude
+
+In their effect on their environment you should be wary of the following:
+* Causing grass to wilt and flowers to close as they pass
+* Animals are alarmed by their presence, particularly dogs
+* Crops wilt and cows' milk turns sour
+* Arguments are stirred within their presence
+* Women's moontide flow becomes more effluent
+* Apparitions and unexplained events occur in their surroundings
+
+Note that a spellweaver will doubtless deny any accusation against them, and will show great emotion when evidence is laid before them. But one should not delay in bringing swift judgement, lest they try to cast a hex or escape by arcane means.]],
+}
+
+newLore{
+	id = "spellhunt-note-3",
+	category = "age of dusk",
+	name = "Spellhunter's Guide part 2: How to Battle a Magic-User",
+	lore = [[Those who rape the forces of nature with their malign wills doubtless unlock great powers to their advantage. We of noble cause, with our abilities aligned to nature's threads, can struggle to equal the terrible might of these unholy mages. But we must persist, for our cause is just, and the threat to this world from the terrors of the arcane evils cannot be under-stated. Too long has Eyal suffered the torture of their presence, and so we must fight with all the powers that nature can give us.
+
+Mobility is key against spell-slinging warlocks and witches. One must rush with full speed towards them and hack them down, not giving them time to utter a spell of attack or defence. Against multiple opponents this could be difficult, and retreat to less open space is often vital. Do not consider this cowardice, for you play the weaker hand and must take all advantage you can acquire. Get into a tighter environment, and keep behind a concealed corner until they are right upon you, before unleashing all of your strength against them without warning.
+
+Necromancers and fell conjurers can present a great threat when they summon their dark minions to overwhelm you. Oft it is best to take out the wizard first, though it presents risk. You must raise your will and suppress all pain until you come right upon him, and then strike him down quickly before retreating to a safer area. Stunning or dazing the caster before they have a chance to summon aid can also help immensely.
+
+Hexes and curses can wreak terror upon you, and you would do well to have an infusion that removes these and other blighted magic effects when facing dread occultists. Infusions to augment your natural healing are also a must for difficult battles.
+
+Remember to show no mercy, for they will give you none.]],
+}
+
+newLore{
+	id = "spellhunt-note-4",
+	category = "age of dusk",
+	name = "Spellhunter's Guide part 3: How to Kill a Magic-User",
+	lore = [[When a magic-user is captured, they must be slain, and slain fast. Captivity is too great a risk. Preferably they should be killed in a way that utterly removes any means for the body to recover through arcane force. Remember that coming back from the dead is no alien feat to these abominations.
+
+Beheading is simple and effective, especially in a hurry, but ideally their other limbs should be removed too and sealed in separate metal cases wrapped in willow-bark.
+
+Devouring by animals is sometimes used, but is not advised, as it can at the very least can cause severe diarrhoea in the animals, and at the worst can leave you facing a pack of fire-breathing hell hounds corrupted by the mana-wielder's aura.
+
+Freezing is ineffective, and no magic-user can ever be frozen to death, in spite of what rumours you may have heard. Anyone you have heard of that was frozen to death was either not a mage or was pretending.
+
+Burning is popular amongst the less educated towns, but typically no witch or wizard of power would ever allow themselves to be caught, bound and put to stake without finding means of escape. Simply putting together the necessary firewood takes too long, even if stocked in advance. Usually those executed in this manner are magic-sympathisers rather than true magic users. Whilst strictly speaking this does not carry the penalty of death it does well to set an example for others and to warn arcane users to stay well away from the community.
+
+The best method, if time and situation allows, is to very slowly slice the caster to death. One should have them securely bound in a sealed room, with willow-bark or beech-wood around their wrists and cranium. One should start by slowly slicing off the toes, fingers and other extremities, before slicing through all the limbs and finally the torso. Do not cut open the skull, in case resident demons escape. The head should be put into a metal box and incinerated for 24 hours with the rest of the remains. Extremely sharp instruments are needed for the job, and do not be surprised if they need re-sharpening several times during the process, as it is a typical warlock's trick to harden their bones before the procedure as a final act of spite.
+
+]],
 }
