962d266f80fb23204dbd23b75d0671c4919c5ca4
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-02-26 02:10:35 +0000

    plop
    git-svn-id: http://svn.net-core.org/repos/t-engine4@2840 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/maps/zones/deep-bellow-last.lua
@@ -0,0 +1,49 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+defineTile("#", {"UNDERGROUND_TREE","UNDERGROUND_TREE2","UNDERGROUND_TREE3","UNDERGROUND_TREE4","UNDERGROUND_TREE5","UNDERGROUND_TREE6","UNDERGROUND_TREE7","UNDERGROUND_TREE8","UNDERGROUND_TREE9","UNDERGROUND_TREE10","UNDERGROUND_TREE11","UNDERGROUND_TREE12","UNDERGROUND_TREE13","UNDERGROUND_TREE14","UNDERGROUND_TREE15","UNDERGROUND_TREE16","UNDERGROUND_TREE17","UNDERGROUND_TREE18","UNDERGROUND_TREE19","UNDERGROUND_TREE20",})
+defineTile(".", "UNDERGROUND_FLOOR")
+defineTile("M", "UNDERGROUND_FLOOR", nil, "THE_MOUTH")
+
+startx = 1
+starty = 14
+endx = 1
+endy = 14
+
+return [[
+###################
+###################
+##########..#######
+####.........######
+###............####
+##..............###
+##.....#........###
+##..........#....##
+##...............##
+##.......M.......##
+##..#.............#
+##...............##
+##...........#...##
+##..............###
+#<..............###
+#.......#......####
+####...........####
+########......#####
+###################
+###################]]
