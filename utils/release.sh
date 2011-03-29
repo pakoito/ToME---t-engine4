da756bb922d876839a5b75a29a68d3b10be6f6f8
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-03-29 16:38:12 +0000

    fix
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3101 51575b47-30f0-44d4-a5cc-537603b46e54

--- utils/release.sh
+++ utils/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if test $# -lt 5 ; then
-	echo "Usage: release.sh [engine version for teae] [tome version for team] [version for public] [long engine version] [long tome version]"
+if test $# -lt 3 ; then
+	echo "Usage: release.sh [engine version for teae] [tome version for team] [version for public]"
 	exit
 fi
 
@@ -15,8 +15,6 @@ fi
 ever="$1"
 tver="$2"
 ver="$3"
-tomename="$4"
-tename="$5"
 
 rm -rf tmp
 mkdir tmp
@@ -30,6 +28,8 @@ echo "******************** Src"
 cd t-engine4-src-"$ver"
 cp -a ../../bootstrap/  ../../game/ ../../C* ../../premake4.lua ../../src/ .
 rm -rf game/modules/angband
+rm -rf game/modules/rogue
+rm -rf game/modules/gruesome
 find . -name '*~' -or -name '.svn' | xargs rm -rf
 
 # create teae/teams
@@ -48,8 +48,11 @@ tar cvjf t-engine4-src-"$ver".tar.bz2 t-engine4-src-"$ver"
 echo "******************** Windows"
 cd t-engine4-windows-"$ver"
 cp -a ../../bootstrap/  ../../game/ ../../C* ../../dlls/* .
+mv te4core-* game/engines/cores/
 find . -name '*~' -or -name '.svn' | xargs rm -rf
 rm -rf game/modules/angband
+rm -rf game/modules/rogue
+rm -rf game/modules/gruesome
 cd ..
 zip -r -9 t-engine4-windows-"$ver".zip t-engine4-windows-"$ver"
 
@@ -57,8 +60,11 @@ zip -r -9 t-engine4-windows-"$ver".zip t-engine4-windows-"$ver"
 echo "******************** linux32"
 cd t-engine4-linux32-"$ver"
 cp -a ../../bootstrap/  ../../game/ ../../C* ../../linux-bin/* .
+mv te4core-* game/engines/cores/
 find . -name '*~' -or -name '.svn' | xargs rm -rf
 rm -rf game/modules/angband
+rm -rf game/modules/rogue
+rm -rf game/modules/gruesome
 cd ..
 tar -cvjf t-engine4-linux32-"$ver".tar.bz2 t-engine4-linux32-"$ver"
 
@@ -67,24 +73,21 @@ tar -cvjf t-engine4-linux32-"$ver".tar.bz2 t-engine4-linux32-"$ver"
 # src
 echo "******************** Src"
 cd t-engine4-src-"$ver"
-rm game/engines/default/data/music/*
-rm game/modules/tome/data/music/*
+IFS=$'\n'; for i in `find game/ -name '*.ogg'`; do rm "$i"; done
 cd ..
 tar cvjf t-engine4-src-"$ver"-nomusic.tar.bz2 t-engine4-src-"$ver"
 
 # windows
 echo "******************** Windows"
 cd t-engine4-windows-"$ver"
-rm game/engines/default/data/music/*
-rm game/modules/tome/data/music/*
+IFS=$'\n'; for i in `find game/ -name '*.ogg'`; do rm "$i"; done
 cd ..
 zip -r -9 t-engine4-windows-"$ver"-nomusic.zip t-engine4-windows-"$ver"
 
 # linux 32
 echo "******************** linux32"
 cd t-engine4-linux32-"$ver"
-rm game/engines/default/data/music/*
-rm game/modules/tome/data/music/*
+IFS=$'\n'; for i in `find game/ -name '*.ogg'`; do rm "$i"; done
 cd ..
 tar -cvjf t-engine4-linux32-"$ver"-nomusic.tar.bz2 t-engine4-linux32-"$ver"
 
@@ -92,17 +95,9 @@ cp *zip *bz2 /var/www/te4.org/htdocs/dl/t-engine
 
 ########## Announce
 
-echo "***************** FOR tome.te4.org"
-echo "== $tomename =="
-echo "* [http://te4.org/dl/t-engine/t-engine4-windows-$ver.zip Windows] ([http://te4.org/dl/t-engine/t-engine4-windows-$ver-nomusic.zip Without music])"
-echo "* [http://te4.org/dl/t-engine/t-engine4-src-$ver.tar.bz2 Source] ([http://te4.org/dl/t-engine/t-engine4-src-$ver-nomusic.tar.bz2 Without music])"
-echo "* [http://te4.org/dl/t-engine/t-engine4-linux32-$ver.tar.bz2 Linux] ([http://te4.org/dl/t-engine/t-engine4-linux32-$ver-nomusic.tar.bz2 Without music])"
-echo "* Mac OSX: not yet available, should be out in a few days (check out the blog)"
-echo
-echo
-echo "***************** FOR te4.org"
-echo "== $tename =="
-echo "* [/dl/t-engine/t-engine4-windows-$ver.zip Windows] ([/dl/t-engine/t-engine4-windows-$ver-nomusic.zip No music])"
-echo "* [/dl/t-engine/t-engine4-src-$ver.tar.bz2 Source] ([/dl/t-engine/t-engine4-src-$ver-nomusic.tar.bz2 No music])"
-echo "* [/dl/t-engine/t-engine4-linux32-$ver.tar.bz2 Linux] ([/dl/t-engine/t-engine4-linux32-$ver-nomusic.tar.bz2 Without music])"
-echo "* Mac OSX: not yet available, should be out in a few days (check out the blog)"
+echo "http://te4.org/dl/t-engine/t-engine4-windows-$ver.zip"
+echo "http://te4.org/dl/t-engine/t-engine4-src-$ver.tar.bz2"
+echo "http://te4.org/dl/t-engine/t-engine4-linux32-$ver.tar.bz2"
+echo "http://te4.org/dl/t-engine/t-engine4-windows-$ver-nomusic.zip"
+echo "http://te4.org/dl/t-engine/t-engine4-src-$ver-nomusic.tar.bz2"
+echo "http://te4.org/dl/t-engine/t-engine4-linux32-$ver-nomusic.tar.bz2"
