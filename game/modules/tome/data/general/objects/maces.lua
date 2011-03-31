f2a54e14cb362e1c42a8ca6b955e825c62ffd590
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-03-31 23:59:38 +0000

    damned
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3132 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/general/objects/maces.lua
+++ game/modules/tome/data/general/objects/maces.lua
@@ -26,7 +26,7 @@ newEntity{
 	encumber = 3,
 	rarity = 5,
 	metallic = true,
-	combat = { talented = "mace", damrange = 1.4, sound = "actions/melee", sound_miss = "actions/melee_miss",},
+	combat = { talented = "mace", damrange = 1.4, physspeed = 1, sound = "actions/melee", sound_miss = "actions/melee_miss",},
 	desc = [[Blunt and deadly.]],
 	randart_able = { attack=40, physical=80, spell=20, def=10, misc=10 },
 	egos = "/data/general/objects/egos/weapon.lua", egos_chance = { prefix=resolvers.mbonus(40, 5), suffix=resolvers.mbonus(40, 5) },
