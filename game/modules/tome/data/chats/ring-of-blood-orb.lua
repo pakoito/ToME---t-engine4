fd270f1db15205440884314ea26efe3a3a23f430
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-04-07 08:12:04 +0000

    Ring of blood orb wont take your money if you killed the blood master
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3181 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/chats/ring-of-blood-orb.lua
+++ game/modules/tome/data/chats/ring-of-blood-orb.lua
@@ -21,7 +21,12 @@ newChat{ id="welcome",
 	text = [[#LIGHT_GREEN#*You place your hands on the orb.*#WHITE#
 You must provide 150 golds to take part in the fight.]],
 	answers = {
-		{"[pay 150 gold]", jump="pay", cond=function(npc, player) return player.money >= 150 end, action=function(npc, player) player:incMoney(-150) end},
+		{"[pay 150 gold]", jump="pay",
+			cond=function(npc, player)
+				return player:hasQuest("ring-of-blood") and player:hasQuest("ring-of-blood"):find_master() and player.money >= 150
+			end,
+			action=function(npc, player) player:incMoney(-150) end
+		},
 		{"[leave]"},
 	}
 }
