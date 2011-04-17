9ef76a19e837e8d7505fc6b24c911157843041cb
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-04-17 21:45:32 +0000

    Timeless wont produce negative timed effects
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3237 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/misc/races.lua
+++ game/modules/tome/data/talents/misc/races.lua
@@ -217,13 +217,18 @@ newTalent{
 	tactical = { DEFEND = 2 },
 	action = function(self, t)
 		local target = self
+		local todel = {}
 		for eff_id, p in pairs(target.tmp) do
 			local e = target.tempeffect_def[eff_id]
 			if e.status == "beneficial" then
 				p.dur = p.dur + self:getTalentLevelRaw(t)
 			elseif e.status == "detrimental" then
 				p.dur = p.dur - self:getTalentLevelRaw(t) * 2
+				if p.dur <= 0 then todel[#todel+1] = eff end
 			end
+		end
+		while #todel > 0 do
+			target:removeEffect(table.remove(todel))
 		end
 
 		game:playSoundNear(self, "talents/spell_generic")
