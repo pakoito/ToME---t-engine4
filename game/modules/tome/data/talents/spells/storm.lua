ec74dc5724079bad9c4b3327e6d5c0f26bf32f3e
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-05-13 21:51:53 +0000

    Fixed Hurricane description
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3347 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/spells/storm.lua
+++ game/modules/tome/data/talents/spells/storm.lua
@@ -126,7 +126,6 @@ newTalent{
 		local radius = t.getRadius(self, t)
 		return ([[Each time one of your lightning spell dazes a target it has %d%% chances to creates a chain reaction that summons a mighty Hurricane that last for 10 turns around the target with radius of %d.
 		Each turn all creatures around it will take %0.2f to %0.2f lightning damage.
-		Only 2 hurricanes can exist at the same time.
 		The damage will increase with the Magic stat]]):format(chance, radius, damage / 3, damage)
 	end,
 }
