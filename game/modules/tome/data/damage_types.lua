ad2984c2c9b45bbf165df50f0bbe8b33e15559c1
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-05-13 22:51:56 +0000

    Blodo Spray correctly handles its disease chance
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3349 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/damage_types.lua
+++ game/modules/tome/data/damage_types.lua
@@ -1033,7 +1033,7 @@ newDamageType{
 		if _G.type(dam) == "number" then dam = {dam=dam} end
 		DamageType:get(DamageType.BLIGHT).projector(src, x, y, DamageType.BLIGHT, dam.dam)
 		local target = game.level.map(x, y, Map.ACTOR)
-		if target and target:canBe("disease") then
+		if target and target:canBe("disease") and rng.percent(dam.disease_chance or 20) then
 			local eff = rng.table{{target.EFF_ROTTING_DISEASE, "con"}, {target.EFF_DECREPITUDE_DISEASE, "dex"}, {target.EFF_WEAKNESS_DISEASE, "str"}}
 			target:setEffect(eff[1], dam.dur or 5, { src = src, [eff[2]] = dam.disease_power or 5, dam = dam.disease_dam or (dam.dam / 5) })
 		end
