a5fd9648e797a284e950f1a4572780ddaa166a48
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-06-27 23:16:26 +0000

    lore paintings
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3750 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/lore/shertul.lua
@@ -0,0 +1,95 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010, 2011 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+--------------------------------------------------------------------------
+-- Sher'Tul
+--------------------------------------------------------------------------
+
+newLore{
+	id = "shertul-fortress-1",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore1.png",
+	lore = [[You see here a mural showing a dark and tortured world. Large, god-like figures with powerful auras fight each other, and the earth is torn beneath their feet.
+There is some text underneath which you do not understand: #{italic}#'Sho ch'zun Eyal mor donuth, ik ranaheli donoth trun ze.'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-2",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore2.png",
+	lore = [[In this picture a huge god with glowing eyes towers above the land, and in his right hand he holds high the sun. The other gods are running from him, wincing from the light. There is some text underneath which you do not understand: #{italic}#'Fa AMAKTHEL tabak, ik koru bazan tro yu, ik ranaheli tobol don schek ruun. Ik blana dem Soli as banafel ik goriz uf Eyal ik blod, "Tro fasa goru domus asam, ik goru domit tro Eyal."'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-3",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore3.png",
+	lore = [[This picture shows the huge god holding some smaller figures in his hands and pointing out at the lands beyond. You imagine these figures must be the Sher'Tul. There is some text beneath which you do not understand: #{italic}#'Ik AMAKTHEL cosio SHER'TUL, ik baladath peris furko masa bren doth benna zi, ik blod is "Fen makel ath goru domus ik denz tro ala fron."'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-4",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore4.png",
+	lore = [[You see a mural showing a huge metropolis made of crystal, with small islands of stone floating in the air behind it. In the foreground is sitting a Sher'Tul, with a hand stretched up to the sky. There is some text beneath which you do not understand: #{italic}#'Batialatoth ro Eyal, ik rinsi akan fronseth sumit kurameth ik linnet pora gasios aeren. Ach nen beswar goreg.'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-5",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore5.png",
+	lore = [[This mural shows nine Sher'Tul standing side by side, each holding aloft a dark weapon. Your eyes are drawn to a runed staff held by the purple-robed figure in the centre. It seems familiar somehow... There is some text beneath which you do not understand: #{italic}#'Zubadon koref noch hesen, ik dorudon koref noch pasor. Cosief maro dondreth karatu \u2013 Ranaduzil \u2013 ik jein belsan ovrienis.'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-6",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore6.png",
+	lore = [[You see images of epic battles, with Sher'Tul warriors fighting and slaying god-like figures over ten times their size. There is some text underneath you do not understand: #{italic}#'Ranaheli meth dondruil ik duzin, ik leisif konru as neremin. Eyal matath bre sun. Ach unu rana soriton\u2026'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-7",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore7.png",
+	lore = [[You see images of epic battles, with Sher'Tul warriors fighting and slaying god-like figures over ten times their size. There is some text underneath you do not understand: #{italic}#'Ranaheli meth dondruil ik duzin, ik leisif konru as neremin. Eyal matath bre sun. Ach unu rana soriton\u2026'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-8",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore8.png",
+	lore = [[The large mural shows the great god spread on the ground, with the dark staff held against his chest. Sher'Tul surround him, some hacking off his limbs, cutting out his tongue, and binding him with chains. A burst of light flares up from where a tall Sher'Tul warrior is gouging his eye with a black-bladed halberd. In the background a Sher'Tul mage beckons to a huge chasm in the ground. The text beneath says simply #{italic}#'Meas Abar.'#{normal}#]],
+}
+
+newLore{
+	id = "shertul-fortress-9",
+	category = "sher'tul",
+	name = "mural painting",
+	image = "shertul_fortress_lore9.png",
+	lore = [[This final mural has been ruined, with deep scores and scratches etched across its surface. All you can see of the original appears to be flames.]],
+}
