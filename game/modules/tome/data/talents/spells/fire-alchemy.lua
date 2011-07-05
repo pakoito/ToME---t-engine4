f5b6d1e0102ccfcff07732592936a1638f874146
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-07-05 16:59:52 +0000

    Much boosted the spel Heat
    git-svn-id: http://svn.net-core.org/repos/t-engine4@3806 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/spells/fire-alchemy.lua
+++ game/modules/tome/data/talents/spells/fire-alchemy.lua
@@ -32,18 +32,18 @@ newTalent{
 	direct_hit = true,
 	tactical = { ATTACK = 2 },
 	requires_target = true,
-	getDamage = function(self, t) return self:combatTalentSpellDamage(t, 25, 220) end,
+	getDamage = function(self, t) return self:combatTalentSpellDamage(t, 25, 620) end,
 	action = function(self, t)
 		local tg = {type="hit", range=self:getTalentRange(t), talent=t}
 		local x, y = self:getTarget(tg)
 		if not x or not y then return nil end
-		self:project(tg, x, y, DamageType.FIREBURN, {dur=5, initial=0, dam=t.getDamage(self, t)}, {type="flame"})
+		self:project(tg, x, y, DamageType.FIREBURN, {dur=8, initial=0, dam=t.getDamage(self, t)}, {type="flame"})
 		game:playSoundNear(self, "talents/fire")
 		return true
 	end,
 	info = function(self, t)
 		local damage = t.getDamage(self, t)
-		return ([[Turn part of your target into fire, burning the rest for %0.2f fire damage over 5 turns.
+		return ([[Turn part of your target into fire, burning the rest for %0.2f fire damage over 8 turns.
 		The damage will increase with Magic stat.]]):format(damDesc(self, DamageType.FIRE, damage))
 	end,
 }
