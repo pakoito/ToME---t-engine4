754f2e3dd0ff49fcf4431f3b3378a52ee0a065ea
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-08-30 08:44:29 +0000

    Cleaner game exit (codewise)
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4297 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/engines/default/modules/boot/class/Game.lua
+++ game/engines/default/modules/boot/class/Game.lua
@@ -325,7 +325,7 @@ function _M:onQuit()
 	if self.is_quitting then return end
 	self.is_quitting = Dialog:yesnoPopup("Quit", "Really exit T-Engine/ToME?", function(ok)
 		self.is_quitting = false
-		if ok then os.exit() end
+		if ok then core.game.exit_engine() end
 	end, "Quit", "Continue")
 end
 
