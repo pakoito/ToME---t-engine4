8021abfcc1f982bb48a82158d48f71f57ff7cbf6
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-09-06 23:23:16 +0000

    Fix a bug with Vew Highscore
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4349 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/engines/default/modules/boot/dialogs/ViewHighScores.lua
+++ game/engines/default/modules/boot/dialogs/ViewHighScores.lua
@@ -69,7 +69,7 @@ function _M:init()
 end
 
 function _M:generateList()
-	local list = Module:listModules()
+	local list = Module:listModules(nil, function(dir) if dir:find("^boot") then return false else return true end end)
 	self.list = {}
 
 	for i = 1, #list do
