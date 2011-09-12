e4822482b91e86f8aa93a3e43259c165f668c4f8
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-09-12 08:38:46 +0000

    test
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4372 51575b47-30f0-44d4-a5cc-537603b46e54

--- utils/te4_pack_module.sh
+++ utils/te4_pack_module.sh
@@ -9,6 +9,23 @@ mod="$1"
 version="$2"
 exclude_ogg="$3"
 
+cd "$mod"
+teams=`lua <<EOS
+local init = loadfile("init.lua")
+local d = {}
+setfenv(init, d)
+init()
+if not d.teams then return end
+for i, def in ipairs(d.teams) do
+	io.write(def[1]..":"..table.concat(def[3],":"):gsub("^/", ""):gsub(":/", ":"))
+	if i < #d.teams then io.write(";") end
+end
+EOS`
+teams=`echo "$teams"| sed "s/#name#/$mod/g" | sed "s/#version#/$version/g"`
+cd ..
+
+echo $teams
+
 cp -a "$mod" tmp
 find tmp -name .svn -or -name '*~' | xargs rm -rf
 cd tmp
@@ -23,6 +40,11 @@ fi
 mkdir mod
 mv * mod
 mv mod/data .
+
+IFS=';'
+for teamdef in `echo "$teams"`; do
+	echo "=== Teamdef: $teamdef"
+end
 
 if test "$exclude_ogg" -eq 1; then
 	zip -r -0 ../"$mod"-"$version-nomusic".team *
