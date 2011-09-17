04dd66897ab4fd28f140923c2f50a6df0167cb3b
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2011-09-17 12:26:55 +0000

    On birth all talents are respecable
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4435 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/dialogs/LevelupDialog.lua
+++ game/modules/tome/dialogs/LevelupDialog.lua
@@ -657,6 +657,7 @@ end
 
 function _M:isUnlearnable(t, limit)
 	if not self.actor.last_learnt_talents then return end
+	if self.on_birth and self.actor:knowTalent(t.id) then return 1 end -- On birth we can reset any talents
 	local list = self.actor.last_learnt_talents[t.generic and "generic" or "class"]
 	local max = self.actor:lastLearntTalentsMax(t.generic and "generic" or "class")
 	local min = 1
