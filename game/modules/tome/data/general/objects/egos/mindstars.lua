169031ef52b0402860db54cc7fd71c5d0efabc25
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-02-29 00:56:49 +0000

    oups
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4950 51575b47-30f0-44d4-a5cc-537603b46e54

---
+++ game/modules/tome/data/general/objects/egos/mindstars.lua
@@ -0,0 +1,33 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010, 2011 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+local Stats = require "engine.interface.ActorStats"
+local Talents = require "engine.interface.ActorTalents"
+
+newEntity{
+	power_source = {arcane=true},
+	name = " of power", suffix=true, instant_resolve=true,
+	keywords = {power=true},
+	level_range = {1, 50},
+	rarity = 4,
+	cost = 8,
+	wielder = {
+		combat_mindpower = resolvers.mbonus_material(16, 3),
+	},
+}
