50b3051ca617e8a1d7bf3124aebb67c7ea0a2c29
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-03-09 23:12:55 +0000

    The Room of Death should not be empty even for low level chars
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4962 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/zones/vor-armoury/npcs.lua
+++ game/modules/tome/data/zones/vor-armoury/npcs.lua
@@ -79,7 +79,8 @@ newEntity{ base="BASE_NPC_ORC_GRUSHNAK", define_as = "GNARG",
 newEntity{ base="GREATER_MULTI_HUED_WYRM", define_as="OVERPOWERED_WYRM",
 	name = "overpowered greater multi-hued wyrm",
 	image = "npc/dragon_multihued_greater_multi_hued_wyrm.png",
-	level_range = {100, nil}, exp_worth = 10,
+	level_range = {100, nil}, exp_worth = 3,
+	wyrm_rarity = 1, rarity = false,
 	rank = 3.5,
 	seen_by = function(self, who)
 		if game:getPlayer(true) ~= who then return end
