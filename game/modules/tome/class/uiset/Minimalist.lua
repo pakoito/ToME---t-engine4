53cbef0c7d7ac614f9eafe1abb5353e1e7cf1337
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-03-22 00:03:42 +0000

    finish
    git-svn-id: http://svn.net-core.org/repos/t-engine4@4981 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/class/uiset/Minimalist.lua
+++ game/modules/tome/class/uiset/Minimalist.lua
@@ -405,12 +405,12 @@ function _M:getMapSize()
 	return 0, 0, w, (self.map_h_stop or 80) - 16
 end
 
-function _M:uiMoveResize(what, button, mx, my, xrel, yrel, bx, by, event, mode, on_change)
+function _M:uiMoveResize(what, button, mx, my, xrel, yrel, bx, by, event, mode, on_change, add_text)
 	if self.locked then return end
 
 	mode = mode or "rescale"
 
-	game.tooltip_x, game.tooltip_y = 1, 1; game:tooltipDisplayAtMap(game.w, game.h, self.mhandle_pos[what].name.."\n---\nLeft mouse drag&drop to move the frame\nRight mouse drag&drop to scale up/down\nMiddle click to reset to default scale")
+	game.tooltip_x, game.tooltip_y = 1, 1; game:tooltipDisplayAtMap(game.w, game.h, self.mhandle_pos[what].name.."\n---\nLeft mouse drag&drop to move the frame\nRight mouse drag&drop to scale up/down\nMiddle click to reset to default scale"..(add_text or ""))
 	if event == "button" and button == "middle" then self.places[what].scale = 1 self:saveSettings()
 	elseif event == "motion" and button == "left" then
 		self.ui_moving = what
@@ -485,16 +485,25 @@ function _M:showResourceTooltip(x, y, w, h, id, desc, is_first)
 				-- Move handle
 				if not self.locked and bx >= self.mhandle_pos.resources.x and bx <= self.mhandle_pos.resources.x + move_handle[6] and by >= self.mhandle_pos.resources.y and by <= self.mhandle_pos.resources.y + move_handle[7] then
 					if event == "button" and button == "right" then
+						local player = game.player
 						local list = {}
-						for id, d in pairs(self.res or {}) do if d.hidable then list[#list+1] = {name=d.hidable, id=id} end end
-						table.sort(list, function(a,b) return a.name<b.name end)
+						if player:knowTalent(player.T_STAMINA_POOL) then list[#list+1] = {name="Stamina", id="stamina"} end
+						if player:knowTalent(player.T_MANA_POOL) then list[#list+1] = {name="Mana", id="mana"} end
+						if player:isTalentActive(player.T_NECROTIC_AURA) then list[#list+1] = {name="Necrotic", id="soul"} end
+						if player:knowTalent(player.T_EQUILIBRIUM_POOL) then list[#list+1] = {name="Equilibrium", id="equilibrium"} end
+						if player:knowTalent(player.T_POSITIVE_POOL) then list[#list+1] = {name="Positive", id="positive"} end
+						if player:knowTalent(player.T_NEGATIVE_POOL) then list[#list+1] = {name="Negative", id="negative"} end
+						if player:knowTalent(player.T_PARADOX_POOL) then list[#list+1] = {name="Paradox", id="paradox"} end
+						if player:knowTalent(player.T_VIM_POOL) then list[#list+1] = {name="Vim", id="vim"} end
+						if player:knowTalent(player.T_HATE_POOL) then list[#list+1] = {name="Hate", id="hate"} end
+						if player:knowTalent(player.T_PSI_POOL) then list[#list+1] = {name="Psi", id="psi"} end
 						Dialog:listPopup("Display/Hide resources", "Toggle:", list, 300, 300, function(sel)
 							if not sel or not sel.id then return end
 							game.player["_hide_resource_"..sel.id] = not game.player["_hide_resource_"..sel.id]
 						end)
 						return
 					end
-					self:uiMoveResize("resources", button, mx, my, xrel, yrel, bx, by, event)
+					self:uiMoveResize("resources", button, mx, my, xrel, yrel, bx, by, event, nil, nil, "\nRight click to toggle resources bars visibility")
 					return
 				end
 			end
