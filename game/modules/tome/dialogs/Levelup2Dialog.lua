99714fb688a74f23bc5738c82d63b76f0e25cd7d
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-06-14 08:24:18 +0000

    fix
    git-svn-id: http://svn.net-core.org/repos/t-engine4@5237 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/dialogs/Levelup2Dialog.lua
+++ game/modules/tome/dialogs/Levelup2Dialog.lua
@@ -495,7 +495,7 @@ function _M:generateList()
 
 	for i, sid in ipairs{self.actor.STAT_STR, self.actor.STAT_DEX, self.actor.STAT_CON, self.actor.STAT_MAG, self.actor.STAT_WIL, self.actor.STAT_CUN } do
 		local s = self.actor.stats_def[sid]
-		local e = engine.Entity.new{image="stats/"..s.short_name:lower()..".png", is_stat=true}
+		local e = engine.Entity.new{image="stats/"..s.name:lower()..".png", is_stat=true}
 		e:getMapObjects(game.uiset.hotkeys_display_icons.tiles, {}, 1)
 
 		local stats = (i <= 3) and phys or mind
