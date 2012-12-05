d4fd481a9e621d0fcabf02fc4ca6fabfb006c443
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-12-05 08:52:44 +0000

    Adventurers start with more equipment and a manasurge rune
    git-svn-id: http://svn.net-core.org/repos/t-engine4@5927 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/birth/classes/adventurer.lua
+++ game/modules/tome/data/birth/classes/adventurer.lua
@@ -90,9 +90,13 @@ newBirthDescriptor{
 			{type="weapon", subtype="dagger", name="iron dagger", autoreq=true, ego_chance=-1000},
 			{type="weapon", subtype="dagger", name="iron dagger", autoreq=true, ego_chance=-1000},
 			{type="weapon", subtype="longsword", name="iron longsword", ego_chance=-1000, ego_chance=-1000},
+			{type="weapon", subtype="longsword", name="iron longsword", ego_chance=-1000, ego_chance=-1000},
 			{type="weapon", subtype="staff", name="elm staff", autoreq=true, ego_chance=-1000},
+			{type="weapon", subtype="mindstar", name="mossy mindstar", autoreq=true, ego_chance=-1000},
+			{type="weapon", subtype="mindstar", name="mossy mindstar", autoreq=true, ego_chance=-1000},
 			{type="armor", subtype="light", name="rough leather armour", ego_chance=-1000, ego_chance=-1000},
 			{type="armor", subtype="cloth", name="linen robe", autoreq=true, ego_chance=-1000},
+			{type="scroll", subtype="rune", name="manasurge rune", ego_chance=-1000, ego_chance=-1000},
 		},
 	},
 }
