824986af22f60ed4138492fd3ec3ce3c24e3aeeb
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-12-09 16:47:09 +0000

    fix
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6019 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/class/interface/PlayerLore.lua
+++ game/modules/tome/class/interface/PlayerLore.lua
@@ -78,7 +78,7 @@ function _M:learnLore(lore, nopopup, silent, nostop)
 	if not l then return end
 	local learnt = false
 
-	if not config.settings.tome.lore_popup and profile.mod.lore and profile.mod.lore.lore and profile.mod.lore.lore[lore] then nopopup = true end
+	if not config.settings.tome.lore_popup and profile.mod.lore and profile.mod.lore.lore and profile.mod.lore.lore[lore] and not l.always_pop then nopopup = true end
 
 	if not self:knownLore(lore) or l.always_pop then
 		game.logPlayer(self, "Lore found: #0080FF#%s", l.name)
