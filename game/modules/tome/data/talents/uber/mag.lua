7c3a1cc042ed1c26efaa26ce533a5f8dcc5704a0
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-12-20 01:50:57 +0000

    Buffed a bit Aether Permeation
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6163 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/uber/mag.lua
+++ game/modules/tome/data/talents/uber/mag.lua
@@ -44,14 +44,14 @@ uberTalent{
 	on_learn = function(self, t)
 		local ret = {}
 		self:talentTemporaryValue(ret, "force_use_resist", DamageType.ARCANE)
-		self:talentTemporaryValue(ret, "force_use_resist_percent", 50)
+		self:talentTemporaryValue(ret, "force_use_resist_percent", 66)
 		return ret
 	end,
 	on_unlearn = function(self, t)
 	end,
 	info = function(self, t)
 		return ([[Create a thin layer of aether all around you. Any attack passing through will check arcane resistance instead of the incoming damage resistance.
-		In effect all your resistances are equal to 50%% of your arcane resistance.]])
+		In effect all your resistances are equal to 66%% of your arcane resistance.]])
 		:format()
 	end,
 }
