e5482caa1c1a1bad7ed547954f48c1762a2d2065
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2012-12-27 21:44:20 +0000

    bump
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6258 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/init.lua
+++ game/modules/tome/init.lua
@@ -22,8 +22,8 @@ long_name = "Tales of Maj'Eyal: Age of Ascendancy"
 short_name = "tome"
 author = { "DarkGod", "darkgod@te4.org" }
 homepage = "http://te4.org/"
-version = {0,9,46}
-engine = {0,9,46,"te4"}
+version = {0,9,47}
+engine = {0,9,47,"te4"}
 description = [[
 Welcome to Maj'Eyal.
 
