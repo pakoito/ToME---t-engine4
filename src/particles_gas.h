43885257f66ba6c8a5c476741440cf26b8239465
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2013-02-20 00:59:41 +0000

    AND MORE!
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6437 51575b47-30f0-44d4-a5cc-537603b46e54

--- src/particles_gas.h
+++ src/particles_gas.h
@@ -1,6 +1,6 @@
 /*
     TE4 - T-Engine 4
-    Copyright (C) 2009, 2010, 2011, 2012 Nicolas Casalini
+    Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
