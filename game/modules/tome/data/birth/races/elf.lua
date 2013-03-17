bac97c09cc78c921ef898e8dc18eeece1a60520c
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2013-03-17 00:25:05 +0000

    Thalore exp penalty back to 35%  (from 30%)
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6567 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/birth/races/elf.lua
+++ game/modules/tome/data/birth/races/elf.lua
@@ -132,6 +132,6 @@ newBirthDescriptor
 		resolvers.inscription("INFUSION:_REGENERATION", {cooldown=10, dur=5, heal=60}),
 		resolvers.inscription("INFUSION:_WILD", {cooldown=12, what={physical=true}, dur=4, power=14}),
 	},
-	experience = 1.30,
+	experience = 1.35,
 	random_escort_possibilities = { {"tier1.1", 1, 2}, {"tier1.2", 1, 2}, {"daikara", 1, 2}, {"old-forest", 1, 4}, {"dreadfell", 1, 8}, {"reknor", 1, 2}, },
 }
