9934b75f5e702aafdda971435e1933437907d2a4
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2013-03-18 14:05:56 +0000

    Lowered Shalore exp penalty to 25%
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6575 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/birth/races/elf.lua
+++ game/modules/tome/data/birth/races/elf.lua
@@ -96,7 +96,7 @@ newBirthDescriptor
 		resolvers.inscription("RUNE:_SHIELDING", {cooldown=14, dur=5, power=100}),
 		resolvers.inscription("RUNE:_PHASE_DOOR", {cooldown=7, range=10, dur=5, power=15}),
 	},
-	experience = 1.35,
+	experience = 1.25,
 	random_escort_possibilities = { {"tier1.1", 1, 2}, {"tier1.2", 1, 2}, {"daikara", 1, 2}, {"old-forest", 1, 4}, {"dreadfell", 1, 8}, {"reknor", 1, 2}, },
 }
 
