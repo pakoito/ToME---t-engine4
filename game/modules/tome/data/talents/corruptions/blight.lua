bf0158389b82335560b01578e459927e8934c828
dg <dg@51575b47-30f0-44d4-a5cc-537603b46e54>
2013-05-19 17:04:49 +0000

    plop
    git-svn-id: http://svn.net-core.org/repos/t-engine4@6742 51575b47-30f0-44d4-a5cc-537603b46e54

--- game/modules/tome/data/talents/corruptions/blight.lua
+++ game/modules/tome/data/talents/corruptions/blight.lua
@@ -105,8 +105,8 @@ newTalent{
 		return true
 	end,
 	info = function(self, t)
-		return ([[Project a corrupted blast of power that deals %0.2f blight damage and removes %d magical or physical effects from any creatures caught in the radius 3 ball.
-		For each removed effect the create has a chance to resist based on its spell save.
+		return ([[Project a corrupted blast of power that deals %0.2f blight damage and removes up to %d magical or physical effects from any creatures caught in the radius 3 ball.
+		For each effect the creature has a chance to resist based on its spell save.
 		The damage will increase with your Spellpower.]]):format(damDesc(self, DamageType.BLIGHT, self:combatTalentSpellDamage(t, 28, 120)), self:getTalentLevelRaw(t))
 	end,
 }
