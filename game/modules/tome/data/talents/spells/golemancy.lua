1959a4ef08c82b6bea428731957b615e059bf68f
DarkGod <darkgod@net-core.org>
2013-06-24 11:09:31 +0200

    Golem Power now also adds mastery with staves, daggers and exotic weapons (but not mindstars)

--- game/modules/tome/data/talents/spells/golemancy.lua
+++ game/modules/tome/data/talents/spells/golemancy.lua
@@ -288,11 +288,17 @@ newTalent{
 		end
 
 		self.alchemy_golem:learnTalent(Talents.T_WEAPON_COMBAT, true, nil, {no_unlearn=true})
+		self.alchemy_golem:learnTalent(Talents.T_STAFF_MASTERY, true, nil, {no_unlearn=true})
+		self.alchemy_golem:learnTalent(Talents.T_KNIFE_MASTERY, true, nil, {no_unlearn=true})
 		self.alchemy_golem:learnTalent(Talents.T_WEAPONS_MASTERY, true, nil, {no_unlearn=true})
+		self.alchemy_golem:learnTalent(Talents.T_EXOTIC_WEAPONS_MASTERY, true, nil, {no_unlearn=true})
 	end,
 	on_unlearn = function(self, t)
 		self.alchemy_golem:unlearnTalent(Talents.T_WEAPON_COMBAT, nil, nil, {no_unlearn=true})
+		self.alchemy_golem:unlearnTalent(Talents.T_STAFF_MASTERY, nil, nil, {no_unlearn=true})
+		self.alchemy_golem:unlearnTalent(Talents.T_KNIFE_MASTERY, nil, nil, {no_unlearn=true})
 		self.alchemy_golem:unlearnTalent(Talents.T_WEAPONS_MASTERY, nil, nil, {no_unlearn=true})
+		self.alchemy_golem:unlearnTalent(Talents.T_EXOTIC_WEAPONS_MASTERY, nil, nil, {no_unlearn=true})
 
 		if self:getTalentLevelRaw(t) == 0 and not self.innate_alchemy_golem then
 			self:unlearnTalent(self.T_REFIT_GOLEM)
