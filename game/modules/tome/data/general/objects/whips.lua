36263a0e6dbd43f948b4e4fa992783428bf2e3a9
DarkGod <darkgod@net-core.org>
2013-10-23 21:33:05 +0200

    fix melee

--- game/modules/tome/data/general/objects/whips.lua
+++ game/modules/tome/data/general/objects/whips.lua
@@ -27,7 +27,7 @@ newEntity{
 	encumber = 3,
 	rarity = 5,
 	metallic = true,
-	combat = { talented = "whip", accuracy_effect = "whip", damrange = 1.1, physspeed = 0.8, sound = "actions/whip_hit", sound_miss = "actions/whip_miss",},
+	combat = { talented = "whip", accuracy_effect = "sword", damrange = 1.1, physspeed = 0.8, sound = "actions/whip_hit", sound_miss = "actions/whip_miss",},
 	desc = [[Sharp, long and deadly.]],
 	randart_able = "/data/general/objects/random-artifacts/generic.lua",
 	egos = "/data/general/objects/egos/weapon.lua", egos_chance = { prefix=resolvers.mbonus(40, 5), suffix=resolvers.mbonus(40, 5) },
