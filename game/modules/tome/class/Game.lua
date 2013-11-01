67486cfa44a4c57c659982d13d3a3eaa4a7b70f7
DarkGod <darkgod@net-core.org>
2013-11-02 00:36:23 +0100

    Add new hook "ToME:runDone"

--- game/modules/tome/class/Game.lua
+++ game/modules/tome/class/Game.lua
@@ -88,11 +88,12 @@ end
 
 function _M:run()
 	class:triggerHook{"ToME:run"}
-	return self:runReal()
+	local ret = self:runReal()
+	class:triggerHook{"ToME:runDone"}
+	return ret
 end
 
 function _M:runReal()
-
 	self.delayed_log_damage = {}
 	self.delayed_log_messages = {}
 	self.calendar = Calendar.new("/data/calendar_allied.lua", "Today is the %s %s of the %s year of the Age of Ascendancy of Maj'Eyal.\nThe time is %02d:%02d.", 122, 167, 11)
