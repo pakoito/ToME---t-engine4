ee89c8b0d3d8e726e33bd52934caabf6c401733a
DarkGod <darkgod@net-core.org>
2013-11-01 17:03:28 +0100

    oups

--- game/modules/tome/data/talents/misc/inscriptions.lua
+++ game/modules/tome/data/talents/misc/inscriptions.lua
@@ -936,7 +936,13 @@ newInscription{
 				else
 					target:forceUseTalent(eff[2], {ignore_energy=true})
 				end
+				self:attr("allow_on_heal", 1)
 				self:heal(data.heal + data.inc_stat, t)
+				self:attr("allow_on_heal", -1)
+				if core.shader.active(4) then
+					self:addParticles(Particles.new("shader_shield_temp", 1, {size_factor=1.5, y=-0.3, img="healdark", life=25}, {type="healing", time_factor=6000, beamsCount=15, noup=2.0, beamColor1={0xcb/255, 0xcb/255, 0xcb/255, 1}, beamColor2={0x35/255, 0x35/255, 0x35/255, 1}}))
+					self:addParticles(Particles.new("shader_shield_temp", 1, {size_factor=1.5, y=-0.3, img="healdark", life=25}, {type="healing", time_factor=6000, beamsCount=15, noup=1.0, beamColor1={0xcb/255, 0xcb/255, 0xcb/255, 1}, beamColor2={0x35/255, 0x35/255, 0x35/255, 1}}))
+				end
 			end
 
 			game.level.map:particleEmitter(px, py, 1, "shadow_zone")
