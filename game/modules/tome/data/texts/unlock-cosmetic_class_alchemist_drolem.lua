62128e6ac593bbaf07bfc8c0ae47a40b3ea4e8c8
DarkGod <darkgod@net-core.org>
2013-11-03 19:41:56 +0100

    fixed drolem unlock

--- game/modules/tome/data/texts/unlock-cosmetic_class_alchemist_drolem.lua
+++ game/modules/tome/data/texts/unlock-cosmetic_class_alchemist_drolem.lua
@@ -18,7 +18,7 @@
 -- darkgod@te4.org
 
 return "New Class Option: #LIGHT_GREEN#Drolem",
-[[You have defeated Tannen with your Alchemist and took soem time to figure how he built a mighty Drolem.
+[[You have defeated Tannen with your Alchemist and took some time to figure how he built a mighty Drolem.
 Drolems are dragon-shaped golems able to breath powerful poison at their foes.
 
 You may now create alchemist characters with a drolem instead of a golem.
