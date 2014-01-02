684dec24c140438f697a103d32d8db4979a2374a
DarkGod <darkgod@net-core.org>
2014-01-02 16:42:13 +0100

    plop

---
+++ game/modules/tome/data/achievements/donator.lua
@@ -0,0 +1,44 @@
+-- ToME - Tales of Maj'Eyal
+-- Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+newAchievement{
+	name = "Bronze Donator", id = "BRONZE_DONATOR",
+	desc = [[Donated up to 5 euros to Tales of Maj'Eyal.]],
+	no_difficulty_duplicate = true, no_chat_broadcast = true,
+}
+newAchievement{
+	name = "Silver Donator", id = "SILVER_DONATOR",
+	desc = [[Donated at least 6 euros to Tales of Maj'Eyal.]],
+	no_difficulty_duplicate = true, no_chat_broadcast = true,
+}
+newAchievement{
+	name = "Gold Donator", id = "GOLD_DONATOR",
+	desc = [[Donated at least 16 euros to Tales of Maj'Eyal.]],
+	no_difficulty_duplicate = true, no_chat_broadcast = true,
+}
+newAchievement{
+	name = "Stralite Donator", id = "STRALITE_DONATOR",
+	desc = [[Donated at least 31 euros to Tales of Maj'Eyal.]],
+	no_difficulty_duplicate = true, no_chat_broadcast = true,
+}
+newAchievement{
+	name = "Voratun Donator", id = "VORATUN_DONATOR",
+	desc = [[Donated more than 60 euros to Tales of Maj'Eyal.]],
+	no_difficulty_duplicate = true, no_chat_broadcast = true,
+}
