25f6ae55171ed67a739564b5f76494e8ff3530c4
DarkGod <darkgod@net-core.org>
2014-01-06 13:27:22 +0100

    fix tutorial text

--- game/modules/tome/data/texts/tutorial/talents.lua
+++ game/modules/tome/data/texts/tutorial/talents.lua
@@ -24,7 +24,7 @@ if not game.player.tutored_levels then
 end
 
 return [[You now possess the Shield Pummel and Shield Wall talents.
-Talents show up in the lower right part of the screen with their assigned hotkey.
+Talents show up in the lower left part of the screen with their assigned hotkey.
 You can right-click on a talent to remove it from the list, or you can add talents by pressing 'M' to get the talents list and then pressing a hotkey.
 Hotkeys by default are the 1 to 0 keys, and can also be assigned to items.
 
