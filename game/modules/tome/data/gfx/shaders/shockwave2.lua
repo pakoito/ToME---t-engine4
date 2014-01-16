c91e7e1b0d0b974345ae790502add4d4aa5ca736
DarkGod <darkgod@net-core.org>
2014-01-16 23:10:37 +0100

    fixed

--- game/modules/tome/data/gfx/shaders/shockwave2.lua
+++ game/modules/tome/data/gfx/shaders/shockwave2.lua
@@ -31,7 +31,7 @@ return {
 		shockwaveSpeed = shockwaveSpeed or 5.0,
 		flameIntensity = flameIntensity or 0.5, 
 
-		ellipsoidalFactor = 1.0, --1 is perfect circle, >1 is ellipsoidal
+		ellipsoidalFactor = {1.0, 1.0}, --1 is perfect circle, >1 is ellipsoidal
 		antialiasingRadius = antialiasingRadius or 0.98, --1.0 is no antialiasing, 0.0 - fully smoothed(looks worse)
 	},
 	resetargs = {
