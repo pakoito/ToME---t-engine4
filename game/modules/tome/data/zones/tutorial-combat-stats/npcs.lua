90aa950d0971291aecd4f1a0986b7a739fbbbbff
DarkGod <darkgod@net-core.org>
2014-01-16 17:52:42 +0100

    big ass commit that does not actaully change a single thing yay

--- game/modules/tome/data/zones/tutorial-combat-stats/npcs.lua
+++ game/modules/tome/data/zones/tutorial-combat-stats/npcs.lua
@@ -1,5 +1,5 @@
 -- ToME - Tales of Maj'Eyal
--- Copyright (C) 2009, 2010, 2011, 2012, 2013 Nicolas Casalini
+-- Copyright (C) 2009 - 2014 Nicolas Casalini
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -533,4 +533,4 @@ newEntity{ base = "BASE_NPC_ORC",
 	combat_physcrit = 50,
 	never_move = 1,
 	combat = false,
-}
+}
