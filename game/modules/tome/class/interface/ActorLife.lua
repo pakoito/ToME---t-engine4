d19eb3732bdcfff30887a59f5beda1498a11a054
DarkGod <darkgod@net-core.org>
2014-06-22 23:25:55 +0200

    Fixed Unstoppable

--- game/modules/tome/class/interface/ActorLife.lua
+++ game/modules/tome/class/interface/ActorLife.lua
@@ -38,6 +38,7 @@ end
 -- @return true/false if the actor died and the actual damage done
 function _M:takeHit(value, src, death_note)
 	if self.onTakeHit then value = self:onTakeHit(value, src, death_note) end
+	if value <= 0 then return false, 0 end
 	self.life = self.life - value
 	self.changed = true
 	if self.life <= self.die_at and not self.dead then
