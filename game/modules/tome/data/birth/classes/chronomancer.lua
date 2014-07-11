4a3c8082cf3fdb70cde2bd96d5e8e49a6d4d4438
Eric Wykoff <ericwykoff@yahoo.com>
2014-07-11 14:10:25 -0500

    teach player about warden weapon swaps

--- game/modules/tome/data/birth/classes/chronomancer.lua
+++ game/modules/tome/data/birth/classes/chronomancer.lua
@@ -148,15 +148,15 @@ newBirthDescriptor{
 	copy = {
 		max_life = 100,
 		resolvers.equipbirth{ id=true,
-			{type="weapon", subtype="longsword", name="iron longsword", autoreq=true, ego_chance=-1000},
-			{type="weapon", subtype="dagger", name="iron dagger", autoreq=true, ego_chance=-1000},
+			{type="weapon", subtype="longbow", name="elm longbow", autoreq=true, ego_chance=-1000},
+			{type="ammo", subtype="arrow", name="quiver of elm arrows", autoreq=true, ego_chance=-1000},
 			{type="armor", subtype="light", name="rough leather armour", autoreq=true, ego_chance=-1000},
 		},
 		resolvers.inventorybirth{ id=true, inven="QS_MAINHAND",
-			{type="weapon", subtype="longbow", name="elm longbow", autoreq=true, ego_chance=-1000},
+			{type="weapon", subtype="longsword", name="iron longsword", autoreq=true, ego_chance=-1000},
 		},
-		resolvers.inventorybirth{ id=true, inven="QS_QUIVER",
-			{type="ammo", subtype="arrow", name="quiver of elm arrows", autoreq=true, ego_chance=-1000},
+		resolvers.inventorybirth{ id=true, inven="QS_OFFHAND",
+			{type="weapon", subtype="dagger", name="iron dagger", autoreq=true, ego_chance=-1000},	
 		},
 		resolvers.generic(function(e)
 			e.auto_shoot_talent = e.T_SHOOT
